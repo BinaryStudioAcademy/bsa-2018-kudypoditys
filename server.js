26c6cade5dacbd0d7f0b20bea4324eb88441fe4f
Dmytro Kostiuk <undefeat.eridan@gmail.com>
2018-08-01 09:40:19 +0300

    Create sample react app

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const
+    path = require('path'),
     express = require('express');
 
 const 
@@ -6,9 +7,7 @@ const
     port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-app.get('/', (req, res) => {
-    res.send('Frontend');
-});
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(port, ip, () => {
     console.log('Server running on http://%s:%s', ip, port);
