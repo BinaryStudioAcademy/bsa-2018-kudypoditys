9b842f05ed632ea5ac0500b8dfdaee760dc0626d
Dmytro Kostiuk <undefeat.eridan@gmail.com>
2018-08-02 11:55:38 +0300

    Change IP to 127.0.0.1

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const
 
 const 
     app = express(),
-    port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+    port = process.env.PORT || 8080,
+    ip = process.env.IP || '127.0.0.1';
 
 app.use(express.static(path.join(__dirname, 'build')));
 
