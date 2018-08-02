935199de7f3bccc9cfe00ef965e9a254bb61bfb1
Dmytro Kostiuk <undefeat.eridan@gmail.com>
2018-08-02 18:46:42 +0300

    Add body-parser and compression.

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const
     path = require('path'),
     express = require('express'),
-    cors = require('cors');
+    cors = require('cors'),
+    bodyParser = require('body-parser')
+    compression = require('compression')
 
 const 
     app = express(),
     port = process.env.PORT || 5000;
 
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 let hitCount = 0;
