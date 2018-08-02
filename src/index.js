2895599070fabded90a94b69f9bf7b7b6610d91d
Dmytro Kostiuk <undefeat.eridan@gmail.com>
2018-08-02 10:55:44 +0300

    Load Semantic UI from CDN.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'whatwg-fetch';
 
-import 'semantic-ui/dist/semantic.css';
+import './styles/global.css';
 
 import App from './components/app';
 
