0038c5a8e22cfbea5ba0d033db3793cad906a92f
fediuchkoelvira <fediuchko.elvira@gmail.com>
2018-08-05 22:32:34 +0300

    Added Property list item component.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,82 +1,60 @@
+// you can use this file to add your custom webpack plugins, loaders and anything you like.
+// This is just the basic way to add additional webpack configurations.
+// For more information refer the docs: https://storybook.js.org/configurations/custom-webpack-config
+
+// IMPORTANT
+// When you add this file, we won't add the default configurations which is similar
+// to "React Create App". This only has babel loader to load JavaScript.
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
-    plugins: [
-        // your custom plugins
-    ],
-    module: {
-        rules: [
-            {
-                oneOf: [
-                    {
-                        test: /\.css$/,
-                        use: [
-                            require.resolve('style-loader'),
-                            {
-                                loader: require.resolve('css-loader'),
-                                options: {
-                                    importLoaders: 1,
-                                },
-                            },
-                            {
-                                loader: require.resolve('postcss-loader'),
-                                options: {
-                                    // Necessary for external CSS imports to work
-                                    // https://github.com/facebookincubator/create-react-app/issues/2677
-                                    ident: 'postcss',
-                                    plugins: () => [
-                                        require('postcss-flexbugs-fixes'),
-                                        autoprefixer({
-                                            browsers: [
-                                                '>1%',
-                                                'last 4 versions',
-                                                'Firefox ESR',
-                                                'not ie < 9', // React doesn't support IE8 anyway
-                                            ],
-                                            flexbox: 'no-2009',
-                                        }),
-                                    ],
-                                },
-                            },
+  plugins: [
+    // your custom plugins
+  ],
+  module: {
+    rules: [
+      // add your custom rules.
+  // add your custom rules.
+  {
+    oneOf: [
+
+        {
+            test: /\.scss$/,
+            use: [
+                require.resolve('style-loader'),
+                {
+                    loader: require.resolve('css-loader'),
+                    options: {
+                        importLoaders: 1,
+                    },
+                },
+                {
+                    loader: require.resolve('postcss-loader'),
+                    options: {
+                        // Necessary for external CSS imports to work
+                        // https://github.com/facebookincubator/create-react-app/issues/2677
+                        ident: 'postcss',
+                        plugins: () => [
+                            require('postcss-flexbugs-fixes'),
+                            autoprefixer({
+                                browsers: [
+                                    '>1%',
+                                    'last 4 versions',
+                                    'Firefox ESR',
+                                    'not ie < 9', // React doesn't support IE8 anyway
+                                ],
+                                flexbox: 'no-2009',
+                            }),
                         ],
                     },
-                    {
-                        test: /\.scss$/,
-                        use: [
-                            require.resolve('style-loader'),
-                            {
-                                loader: require.resolve('css-loader'),
-                                options: {
-                                    importLoaders: 1,
-                                },
-                            },
-                            {
-                                loader: require.resolve('postcss-loader'),
-                                options: {
-                                    // Necessary for external CSS imports to work
-                                    // https://github.com/facebookincubator/create-react-app/issues/2677
-                                    ident: 'postcss',
-                                    plugins: () => [
-                                        require('postcss-flexbugs-fixes'),
-                                        autoprefixer({
-                                            browsers: [
-                                                '>1%',
-                                                'last 4 versions',
-                                                'Firefox ESR',
-                                                'not ie < 9', // React doesn't support IE8 anyway
-                                            ],
-                                            flexbox: 'no-2009',
-                                        }),
-                                    ],
-                                },
-                            },
-                            {
-                                loader: require.resolve('sass-loader'),
-                            }
-                        ],
-                    }
-                ]
-            }
-        ],
-    },
+                },
+                {
+                    loader: require.resolve('sass-loader'),
+                }
+            ],
+        }
+    ]
+}
+],
+  },
 };
--- src/client/components/PropertyListItem/index.jsx
+++ src/client/components/PropertyListItem/index.jsx
@@ -1,114 +1,141 @@
 import React from 'react';
-import { Card, Image, Button, CardContent, Grid, Container, Icon, Header, Label, Content } from 'semantic-ui-react';
+import { Card, Image, Button, CardContent, Grid, CardMeta, CardDescription, Container, Icon, Header, Label } from 'semantic-ui-react';
+import './index.scss';
 
 export default class PropertyListItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            image: `http://cdn.home-designing.com/wp-content/uploads/2016/04/luxury-art-deco-apartment-interior.jpg`,
+            name: 'DREAM Hostel Lviv',
+            description: 'Це помешкання розташоване в 6 хв. ходьби від пляжу Історичний готель оформлений у класичному стилі та розташований за 10 хвилин ходьби від вулиці Дерибасівська, Потьомкінських сходів та памятника герцогу де Рішельє і за 300 метрів від театру опери та балету. Із закладу відкривається чудовий вид на Чорне море.Це помешкання розташоване в 6 хв. ходьби від пляжу Історичний готель оформлений у класичному стилі та розташований за 10 хвилин ходьби від вулиці Дерибасівська, Потьомкінських сходів та памятника герцогу де Рішельє і за 300 метрів від театру опери та балету. Із закладу відкривається чудовий вид на Чорне море.',
+            rating: '9.7',
+            location: 'Lviv',
+            distanceToCenter: '1.5',
+            priceTo: '500',
+            priceFrom: '700',
+            curency: 'uah',
+            reviewsNamber: '660',
+            locationRating: '9.2'
+        }
+    }
+    handleRedirectToMap = id => {
+        //todo  add redirection to map
+    }
+    handleAddToComparison = id => {
+        // todo sdd to comparision
+    }
+    handleAddToFavorites = id => {
+        // todo add to favorites
+    }
+    handleRedirectToDetails = id => {
+        //todo add redirection to property page
+    }
     render() {
-        let propertyImg = `http://cdn.home-designing.com/wp-content/uploads/2016/04/luxury-art-deco-apartment-interior.jpg`;
         return (
-            <Card
-                fluid
+            <Card fluid
                 style={{
                     margin: "0.5rem",
                     padding: "0.5rem",
                     border: "groove"
                 }}>
                 <CardContent>
-                    <Grid celled>
+                    <Grid>
                         <Grid.Column width={3}>
                             <Image
                                 rounded
-                                src={propertyImg}
-                                className="listItem-description"
-                                floated="left"
-                            />
+                                src={this.state.image}
+                                floated="left" />
                         </Grid.Column>
                         <Grid.Column width={9}>
                             <Header as='h3'
                                 style={{ marginTop: "1.5rem" }}
-                                color='blue'                  >
+                                color='blue'
+                                onClick={this.handleRedirectToDetails}        >
                                 <Icon
-                                    name='heart'
                                     size='mini'
                                     color='black'
+                                    name='heart'
+                                    onClick={this.handleAddToFavorites}
                                 />
                                 <Icon
-                                    name='balance scale'
                                     size='tiny'
                                     color='black'
+                                    name='balance scale'
+                                    onClick={this.handleAddToComparison}
                                 />
-                                <Header.Content>Some nice title</Header.Content>
+                                <Header.Content>{this.state.name}</Header.Content>
                             </Header>
-                            <Card.Meta
+                            <CardMeta
                                 style={{
                                     marginTop: "0.5rem"
                                 }}>
-                                <Container>
+                                <Container className="qqq">
                                     <Icon
                                         name='map marker'
                                         size='large'
-                                    />
-                                    <a href=''>Lviv Show on map</a> 1.5 km to center
+                                        onClick={this.handleRedirectToMap} />
+                                    <a href=''>{this.state.location} Show on map</a>  {this.state.distanceToCenter} km to center
                                     </Container>
-                            </Card.Meta>
-                            <Card.Description
+                            </CardMeta>
+                            <CardDescription
                                 textAlign='left'
                                 floated="right"
-                                className='listItem-description'
                                 style={{
                                     marginTop: "0.5rem"
-                                }} >
-                                Це помешкання розташоване в 6 хв. ходьби від пляжу Історичний готель оформлений у класичному стилі та розташований за 10 хвилин ходьби від вулиці Дерибасівська, Потьомкінських сходів та пам'ятника герцогу де Рішельє і за 300 метрів від театру опери та балету. Із закладу відкривається чудовий вид на Чорне море.Це помешкання розташоване в 6 хв. ходьби від пляжу Історичний готель оформлений у класичному стилі та розташований за 10 хвилин ходьби від вулиці Дерибасівська, Потьомкінських сходів та пам'ятника герцогу де Рішельє і за 300 метрів від театру опери та балету. Із закладу відкривається чудовий вид на Чорне море.
-                            </Card.Description>
+                                }}>
+                                {this.state.description}
+                            </CardDescription>
                         </Grid.Column>
-                        <Grid.Column width={2}>
-                            <Grid.Row centered='false'>
-                                <Grid celled  >
-                                    <Grid.Column   width={1} >
-                                        <Header as='h4'
-
-                                            color='blue' >
-                                            <Header.Content>Superb</Header.Content>
-                                        </Header>
-                                        660reviews
-                                    </Grid.Column>
-                                    <Grid.Column width={1} >
-                                        <Label size='big' color='blue'>
-                                            9.2
-                                        </Label>
-                                    </Grid.Column>
-                                </Grid>
-                            </Grid.Row>
-                            <Grid.Row >
-                                <Header as='h5'
-                                    color='blue'
-                                    textAlign="right" >
-                                    <Header.Content>location 9.7</Header.Content>
-                                </Header>
-                            </Grid.Row>
-                            <Grid.Row>
-                                <Container
-                                    floated='right'
-                                    textAlign='right'
-                                    style={{
-                                        marginTop: "8rem",
-
-                                    }}>
-                                    <span>prise from 500 to 700$</span>
-                                    <Button primary
-                                        floated='right'
+                        <Grid.Column width={3}>
+                            <Grid columns={2}>
+                                <Grid.Column >
+                                    <Container textAlign='right'>
+                                       <span className="ratingName"> Superb</span><br />
+                                        {this.state.reviewsNamber} reviews
+                                    </Container>
+                                </Grid.Column>
+                                <Grid.Column>
+                                    <Label size='big' color='blue'>
+                                        {this.state.rating}</Label>
+                                </Grid.Column>
+                            </Grid>
+                            <Grid columns={1}>
+                                <Grid.Column>
+                                    <Header as='h5'
+                                        color='blue'
+                                        textAlign="right" >
+                                        <Header.Content
+                                            style={{
+                                                marginRight: "2.5rem",
+                                            }}>location {this.state.locationRating}</Header.Content>
+                                    </Header>
+                                    <Container floated='right'
+                                        textAlign='right'
                                         style={{
-                                            marginTop: "0.5rem"
-                                        }}
-                                    >Choose room
-                                    </Button>
-                                </Container>
-                            </Grid.Row>
+                                            marginTop: "5rem"
+                                        }}>
+                                        <span
+                                            className="priceInfo">prise from {this.state.priceFrom} to {this.state.priceTo} {this.state.curency}</span>
+                                        <Button primary
+                                            floated='right'
+                                            style={{
+                                                marginTop: "0.5rem"
+                                            }}
+                                            onClick={this.handleRedirectToDetails}
+                                        >Choose room
+                                        </Button>
+                                    </Container>
+                                </Grid.Column>
+                            </Grid>
                         </Grid.Column>
                     </Grid>
                 </CardContent>
             </Card>
-
         )
     }
 }
+
+
+
 
