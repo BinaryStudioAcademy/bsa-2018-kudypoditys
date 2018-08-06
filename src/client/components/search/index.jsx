9b0dd9b32da6ac9ba3fc4775facf94052edac137
Vladyslav Sorochynskyi <vladyslav.sorochynskyi@gmail.com>
2018-08-06 20:11:31 +0300

    submit handle
    parse date range into 2 Date objects

--- src/client/components/search/index.jsx
+++ src/client/components/search/index.jsx
@@ -61,6 +61,92 @@ class SearchComponent extends React.Component {
         return `${this.state.rooms} Rooms`;
     }
 
+    handleSubmit = () => {
+        const { checkIn, checkOut } = this.parseDateRange(this.state.date);
+        const searchValue = {
+            destination: this.state.destination,
+            checkIn: checkIn,
+            checkOut: checkOut,
+            rooms: this.state.rooms,
+            adults: this.state.adults,
+            children: this.state.children
+        }
+        this.props.onSubmit(searchValue);
+    }
+
+    parseDateRange = dateRangeString => {
+        let pos = 4;
+
+        let dateIn='', monthIn='', dateOut='', monthOut='';
+        for (; dateRangeString.charAt(pos) !== ' '; pos++) {
+            dateIn += dateRangeString.charAt(pos);
+        }
+        pos++;
+        for (; dateRangeString.charAt(pos) !== ' '; pos++) {
+            if (pos === dateRangeString.length) break;
+            monthIn += dateRangeString.charAt(pos);
+        }
+        pos += 7;
+        for (; dateRangeString.charAt(pos) !== ' '; pos++) {
+            if (pos >= dateRangeString.length) break;
+            dateOut += dateRangeString.charAt(pos);
+        }
+        pos++;
+        for (; pos<dateRangeString.length; pos++) {
+            if (pos >= dateRangeString.length) break;
+            monthOut += dateRangeString.charAt(pos);
+        }
+        const checkIn = this.getDateObject(dateIn, monthIn);
+        const checkOut = this.getDateObject(dateOut, monthOut);
+
+        return { checkIn, checkOut };
+    }
+
+    getDateObject = (date, month) => {
+        if (date === '' || month === '') return null;
+
+        const currentDate = new Date(Date.now());
+        const currentYear = currentDate.getFullYear();
+        const currentMonth = currentDate.getMonth();
+        month = this.monthStringToNumber(month);
+
+        if (currentMonth < month)
+            return new Date(currentYear + 1, month, date);
+        else
+            return new Date(currentYear, month, date);
+    }
+
+    monthStringToNumber = monthString => {
+        switch(monthString) {
+            case 'Jan':
+                return 0;
+            case 'Feb':
+                return 1;
+            case 'Mar':
+                return 2;
+            case 'Apr':
+                return 3;
+            case 'May':
+                return 4;
+            case 'Jun':
+                return 5;
+            case 'Jul':
+                return 6;
+            case 'Aug':
+                return 7;
+            case 'Sep':
+                return 8;
+            case 'Oct':
+                return 9;
+            case 'Nov':
+                return 10;
+            case 'Dec':
+                return 11;
+            default:
+                return -1;
+        }
+    }
+
     render() {
         const selectOptions = this.generateOptions(1, 10);
         const childrenOptions = this.generateOptions(0, 10);
@@ -68,7 +154,7 @@ class SearchComponent extends React.Component {
 
         return (
             view === 'bar' ?
-            <Form className='search search--view-bar' >
+            <Form className='search search--view-bar' onSubmit={this.handleSubmit}>
                 <div className='destination'>
                     <Input
                         iconPosition='left'
@@ -77,11 +163,14 @@ class SearchComponent extends React.Component {
                         placeholder='Where are you going?'
                         onChange={this.handleChange}
                         onFocus={this.hideRoomSelector}
+                        required
                     />
                 </div>
                 <div className='check-in-out'>
                 <DatesRangeInput
                     closable
+                    required
+                    autoComplete='off'
                     minDate={moment()}
                     dateFormat='ddd D MMM'
                     popupPosition='bottom center'
@@ -92,6 +181,7 @@ class SearchComponent extends React.Component {
                     value={this.state.date}
                     onChange={this.handleChange}
                     onFocus={this.hideRoomSelector}
+                    onKeyPress={event => event.preventDefault()}
                 />
                 </div>
                 <div className='room-options'>
@@ -153,12 +243,15 @@ class SearchComponent extends React.Component {
                     name='destination'
                     placeholder='Where are you going?'
                     onChange={this.handleChange}
+                    required
                 />
             </Form.Field>
             <Form.Field className='check-in-out'>
                 <label>Check-in and Check-out date</label>
                 <DatesRangeInput
                     closable
+                    required
+                    autoComplete='off'
                     minDate={moment()}
                     dateFormat='ddd D MMM'
                     popupPosition='bottom center'
@@ -168,6 +261,7 @@ class SearchComponent extends React.Component {
                     name='date'
                     value={this.state.date}
                     onChange={this.handleChange}
+                    onKeyPress={event => event.preventDefault()}
                 />
             </Form.Field>
             <div className='room-options'>
