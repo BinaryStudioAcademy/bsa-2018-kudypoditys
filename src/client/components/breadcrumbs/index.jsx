520038ef9e40df1b246b3b770294f30eda5fcd63
igorjv123 <igorjv123@gmail.com>
2018-08-06 16:01:17 +0300

    Deleted 'defaultProps' and 'constructor'

--- src/client/components/breadcrumbs/index.jsx
+++ src/client/components/breadcrumbs/index.jsx
@@ -3,17 +3,6 @@ import { Breadcrumb } from 'semantic-ui-react';
 import './index.scss';
 
 class Breadcrumbs extends React.Component {
-    static defaultProps = {
-        sections : [
-            { key: 'Home', content: 'Home', href:'#' },
-            { key: 'Ukraine', content: 'Ukraine', href:'#'},
-            { key: 'Lviv', content: 'Lviv', href:'#'},
-            { key: 'Awesome Apart', content: 'Awesome Apart', active: true},
-          ]
-    };
-    constructor(props){
-        super(props)
-    }
     render() {
         return (
             <Breadcrumb icon='right angle' sections={this.props.sections} />
