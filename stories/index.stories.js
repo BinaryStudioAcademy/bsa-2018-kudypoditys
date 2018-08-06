165831bdd0ca861213984a9813789313998d7a16
Nikolay Datsko <sayber1990@yandex.ru>
2018-08-06 17:35:22 +0300

    Added more stories

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -20,9 +20,33 @@ storiesOf('Button', module)
         </Button>
     ));
 
-let errors = [{ field: 'email', message: 'Please enter your e-mail'}, { field: 'password', message: 'Please enter your password' }]
+let errors = [{ field: 'email', message: 'Please enter your e-mail' }, { field: 'password', message: 'Please enter your password' }];
 
 storiesOf('Login component', module)
-    .add('Login main', () => (
+    .add('Login with errors', () => (
         <LoginComponent email={''} password={''} errors={errors} />
+    ));
+
+storiesOf('Login component', module)
+    .add('Login without errors', () => (
+        <LoginComponent email={''} password={''} errors={[]} />
+    ));
+
+storiesOf('Login component', module)
+    .add('Login with email and password', () => (
+        <LoginComponent email={'testmail@gmail.com'} password={'123456'} errors={[]} />
+    ));
+
+let PasswordLengthError = [{ field: 'password', message: 'Your password must be at least 6 characters' }]
+
+storiesOf('Login component', module)
+    .add('Login with wrong password length', () => (
+        <LoginComponent email={'testmail@gmail.com'} password={'12345'} errors={PasswordLengthError} />
+    ));
+
+let EmailFormatError = [{ field: 'email', message: 'Please enter a valid e-mail' }]
+
+storiesOf('Login component', module)
+    .add('Login with wrong email format', () => (
+        <LoginComponent email={'testmailgmailcom'} password={'12345'} errors={EmailFormatError} />
     ));
