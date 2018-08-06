d6ef7991ceaff474d69c4f2d0e04af7911026b77
fediuchkoelvira <fediuchko.elvira@gmail.com>
2018-08-06 21:20:20 +0300

    Add story  in storybook

---
+++ src/client/components/property-list-item/index.jsx
@@ -0,0 +1,170 @@
+import React from 'react';
+import { Card,Message, Image, Button, CardContent, Grid, CardMeta, CardDescription, Container, Icon, Header, Label } from 'semantic-ui-react';
+import './index.scss';
+import PropTypes from 'prop-types';
+
+class PropertyListItem extends React.Component {
+
+    handleRedirectToMap = id => {
+        //todo  add redirection to map
+    }
+    handleAddToComparison = id => {
+        // todo sdd to comparision
+    }
+    handleAddToFavorites = id => {
+        // todo add to favorites
+    }
+    handleRedirectToDetails = id => {
+        //todo add redirection to property page
+    }
+    render() {
+        const {propertyItemData}=this.props
+
+        let ratingStatus = ''
+        if (propertyItemData.rating >= 9) {
+            ratingStatus = 'Excellent'
+        } else if (propertyItemData.rating >= 7) {
+            ratingStatus = 'Very Good'
+        } else if (propertyItemData.rating >= 5) {
+            ratingStatus = 'Good'
+        } else if (propertyItemData.rating >= 1) {
+            ratingStatus = 'Satisfactorily'
+        }
+
+        return (
+            <Card fluid
+                style={{
+                    margin: "0.5rem",
+                    padding: "0.5rem",
+                    border: "groove"
+                }}>
+                <CardContent>
+                    <Grid>
+                        <Grid.Column width={3}>
+                        <Label as='a' color='orange' ribbon style={{display:propertyItemData.mealType===undefined?'none':'block'}}>
+                        {propertyItemData.mealType}}
+                        </Label>
+                            <Image
+                                rounded
+                                src={propertyItemData.image}
+                                floated="left" />
+                        </Grid.Column>
+                        <Grid.Column width={9}>
+                            <Header as='h3'
+                                style={{ marginTop: "1.5rem" }}
+                                color='blue'
+                                onClick={this.handleRedirectToDetails}        >
+                                <Icon
+                                    size='mini'
+                                    color='black'
+                                    name='heart'
+                                    onClick={this.handleAddToFavorites}
+                                />
+                                <Icon
+                                    size='tiny'
+                                    color='black'
+                                    name='balance scale'
+                                    onClick={this.handleAddToComparison}
+                                />
+                                <Header.Content>{propertyItemData.name}</Header.Content>
+                            </Header>
+                            <CardMeta
+                                style={{
+                                    marginTop: "0.5rem"
+                                }}>
+                                <Container className="qqq">
+                                    <Icon
+                                        name='map marker'
+                                        size='large'
+                                        onClick={this.handleRedirectToMap} />
+                                    <a href=''>{propertyItemData.location} Show on map</a>  {propertyItemData.distanceToCenter} km to center
+                                    </Container>
+                            </CardMeta>
+                            <CardDescription
+                                textAlign='left'
+                                floated="right"
+                                style={{
+                                    marginTop: "0.5rem"
+                                }}>
+                                {propertyItemData.description}
+                            </CardDescription>
+                            <Message style={{
+                                          color:'red',
+                                          display: propertyItemData.availableRoomsCount===0?'block':'none'
+                                        }}>
+                              unfortunately we do not have any available rooms yet
+                           </Message>
+                        </Grid.Column>
+                        <Grid.Column width={3}>
+                            <Grid columns={2}>
+                                <Grid.Column >
+                                    <Container textAlign='right'>
+                                        <span className="ratingName"> {ratingStatus}</span><br />
+                                        {propertyItemData.reviewsNamber} reviews
+                                    </Container>
+                                </Grid.Column>
+                                <Grid.Column>
+                                    <Label size='big' color={propertyItemData.rating>5?'blue':'grey'}>
+                                        {propertyItemData.rating}</Label>
+                                </Grid.Column>
+                            </Grid>
+                            <Grid columns={1}>
+                                <Grid.Column>
+                                    <Header as='h5'
+                                        color='blue'
+                                        textAlign="right" >
+                                        <Header.Content
+                                            style={{
+                                                marginRight: "2.5rem",
+                                            }}>location {propertyItemData.locationRating}</Header.Content>
+                                    </Header>
+                                    <Container floated='right'
+                                        textAlign='right'
+                                        style={{
+                                            marginTop: "5rem"
+                                        }}>
+                                        <span
+                                            className="priceInfo">prise from {propertyItemData.priceFrom} to {propertyItemData.priceTo} {propertyItemData.curency}</span>
+
+                                        <Button color ={propertyItemData.availableRoomsCount===0?'grey':'blue'}
+                                            floated='right'
+                                            style={{
+                                                marginTop: "0.5rem",
+
+                                               // display: 'none'
+                                            }}
+                                            onClick={propertyItemData.availableRoomsCount===0?'':this.handleRedirectToDetails}
+                                        >Choose room
+                                        </Button>
+                                    </Container>
+                                </Grid.Column>
+                            </Grid>
+                        </Grid.Column>
+                    </Grid>
+                </CardContent>
+            </Card>
+        )
+    }
+}
+PropertyListItem.propTypes = {
+    propertyItemData: PropTypes.shape({
+        image: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        rating: PropTypes.number.isRequired,
+        location: PropTypes.string.isRequired,
+        distanceToCenter: PropTypes.number.isRequired,
+        priceTo: PropTypes.number.isRequired,
+        priceFrom: PropTypes.number.isRequired,
+        curency: PropTypes.string.isRequired,
+        reviewsNamber: PropTypes.number.isRequired,
+        locationRating: PropTypes.number.isRequired,
+        availableRoomsCount:PropTypes.number.isRequired,
+        mealType:PropTypes.string
+    })
+};
+
+export default PropertyListItem;
+
+
+
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -5,7 +5,7 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
-import PropertyListItem from '../src/client/components/PropertyListItem';
+import PropertyListItem from '../src/client/components/property-list-item';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
@@ -30,6 +30,88 @@ let propertyItemData = {
     priceFrom: 700,
     curency: 'uah',
     reviewsNamber: 660,
-    locationRating: 9.2
+    locationRating: 9.2,
+    availableRoomsCount:4
 }
-storiesOf('Property Item', module).add('Item base state', () => <PropertyListItem propertyItemData={propertyItemData} />);
+
+let propertyItemData1 = {
+    image: `http://cdn.home-designing.com/wp-content/uploads/2016/04/luxury-art-deco-apartment-interior.jpg`,
+    name: 'DREAM Hostel Lviv',
+    description: 'Це помешкання розташоване в 6 хв. ходьби від пляжу Історичний готель оформлений у класичному стилі та розташований за 10 хвилин ходьби від вулиці Дерибасівська, Потьомкінських сходів та памятника герцогу де Рішельє і за 300 метрів від театру опери та балету. Із закладу відкривається чудовий вид на Чорне море.Це помешкання розташоване в 6 хв. ходьби від пляжу Історичний готель оформлений у класичному стилі та розташований за 10 хвилин ходьби від вулиці Дерибасівська, Потьомкінських сходів та памятника герцогу де Рішельє і за 300 метрів від театру опери та балету. Із закладу відкривається чудовий вид на Чорне море.',
+    rating: 4.2,
+    location: 'Lviv',
+    distanceToCenter: 1.2,
+    priceTo: 500,
+    priceFrom: 700,
+    curency: 'uah',
+    reviewsNamber: 660,
+    locationRating: 9.2,
+    availableRoomsCount:2
+}
+
+let propertyItemData2 = {
+    image: `http://cdn.home-designing.com/wp-content/uploads/2016/04/luxury-art-deco-apartment-interior.jpg`,
+    name: 'DREAM Hostel Lviv',
+    description: 'Це помешкання розташоване в 6 хв. ходьби від пляжу Історичний готель оформлений у класичному стилі та розташований за 10 хвилин ходьби від вулиці Дерибасівська, Потьомкінських сходів та памятника герцогу де Рішельє і за 300 метрів від театру опери та балету. Із закладу відкривається чудовий вид на Чорне море.Це помешкання розташоване в 6 хв. ходьби від пляжу Історичний готель оформлений у класичному стилі та розташований за 10 хвилин ходьби від вулиці Дерибасівська, Потьомкінських сходів та памятника герцогу де Рішельє і за 300 метрів від театру опери та балету. Із закладу відкривається чудовий вид на Чорне море.',
+    rating: 9.7,
+    location: 'Lviv',
+    distanceToCenter: 1.2,
+    priceTo: 500,
+    priceFrom: 700,
+    curency: 'uah',
+    reviewsNamber: 660,
+    locationRating: 9.2,
+    availableRoomsCount: 0
+}
+let propertyItemData3 = {
+    image: `http://cdn.home-designing.com/wp-content/uploads/2016/04/luxury-art-deco-apartment-interior.jpg`,
+    name: 'DREAM Hostel Lviv',
+    description: 'Це помешкання розташоване в 6 хв. ходьби від пляжу Історичний готель оформлений у класичному стилі та розташований за 10 хвилин ходьби від вулиці Дерибасівська, Потьомкінських сходів та памятника герцогу де Рішельє і за 300 метрів від театру опери та балету. Із закладу відкривається чудовий вид на Чорне море.Це помешкання розташоване в 6 хв. ходьби від пляжу Історичний готель оформлений у класичному стилі та розташований за 10 хвилин ходьби від вулиці Дерибасівська, Потьомкінських сходів та памятника герцогу де Рішельє і за 300 метрів від театру опери та балету. Із закладу відкривається чудовий вид на Чорне море.',
+    rating: 4.2,
+    location: 'Lviv',
+    distanceToCenter: 1.2,
+    priceTo: 500,
+    priceFrom: 700,
+    curency: 'uah',
+    reviewsNamber: 660,
+    locationRating: 9.2,
+    availableRoomsCount: 7,
+    mealType:'Breakfast included'
+}
+let propertyItemData4 = {
+    image: `http://cdn.home-designing.com/wp-content/uploads/2016/04/luxury-art-deco-apartment-interior.jpg`,
+    name: 'DREAM Hostel Lviv',
+    description: 'Це помешкання розташоване в 6 хв. ходьби від пляжу Історичний готель оформлений у класичному стилі та розташований за 10 хвилин ходьби від вулиці Дерибасівська, Потьомкінських сходів та памятника герцогу де Рішельє і за 300 метрів від театру опери та балету. Із закладу відкривається чудовий вид на Чорне море.Це помешкання розташоване в 6 хв. ходьби від пляжу Історичний готель оформлений у класичному стилі та розташований за 10 хвилин ходьби від вулиці Дерибасівська, Потьомкінських сходів та памятника герцогу де Рішельє і за 300 метрів від театру опери та балету. Із закладу відкривається чудовий вид на Чорне море.',
+    rating: 4.2,
+    location: 'Lviv',
+    distanceToCenter: 1.2,
+    priceTo: 500,
+    priceFrom: 700,
+    curency: 'uah',
+    reviewsNamber: 660,
+    locationRating: 9.2,
+    availableRoomsCount: 5,
+    mealType:'All meals included'
+}
+let propertyItemData5 = {
+    image: `http://cdn.home-designing.com/wp-content/uploads/2016/04/luxury-art-deco-apartment-interior.jpg`,
+    name: 'DREAM Hostel Lviv',
+    description: 'Це помешкання розташоване в 6 хв. ходьби від пляжу Історичний готель оформлений у класичному стилі та розташований за 10 хвилин ходьби від вулиці Дерибасівська, Потьомкінських сходів та памятника герцогу де Рішельє і за 300 метрів від театру опери та балету. Із закладу відкривається чудовий вид на Чорне море.Це помешкання розташоване в 6 хв. ходьби від пляжу Історичний готель оформлений у класичному стилі та розташований за 10 хвилин ходьби від вулиці Дерибасівська, Потьомкінських сходів та памятника герцогу де Рішельє і за 300 метрів від театру опери та балету. Із закладу відкривається чудовий вид на Чорне море.',
+    rating: 9.7,
+    location: 'Lviv',
+    distanceToCenter: 1.2,
+    priceTo: 500,
+    priceFrom: 700,
+    curency: 'uah',
+    reviewsNamber: 660,
+    locationRating: 9.2,
+    availableRoomsCount: 1
+}
+storiesOf('Property Item', module)
+.add('Item base state', () => <PropertyListItem propertyItemData={propertyItemData} />)
+.add ('Item with low raitung',() => <PropertyListItem propertyItemData={propertyItemData1} /> )
+.add ('No available room',() => <PropertyListItem propertyItemData={propertyItemData2} /> )
+.add ('Item with breakfast included',() => <PropertyListItem propertyItemData={propertyItemData3} /> )
+.add ('Item with all meals included',() => <PropertyListItem propertyItemData={propertyItemData4} /> )
+.add ('Last available room',() => <PropertyListItem propertyItemData={propertyItemData5} /> )
+
