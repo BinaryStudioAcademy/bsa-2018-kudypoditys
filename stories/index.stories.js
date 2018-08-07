be42fd75201c5be1986f34cfd7149c7fb515be99
Vladyslav Sorochynskyi <vladyslav.sorochynskyi@gmail.com>
2018-08-07 16:57:31 +0300

    stories in separate file

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -6,8 +6,6 @@ import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
 
-import SearchComponent from '../src/client/components/search/index';
-
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
@@ -20,35 +18,3 @@ storiesOf('Button', module)
         </Button>
     ));
 
-storiesOf('Search', module)
-    .add('search bar with values', () => (
-        <SearchComponent
-            view='bar'
-            destination='Lviv'
-            checkIn={new Date('Aug 14 2018')}
-            checkOut={new Date('Aug 16 2018')}
-            adults={1}
-            rooms={1}
-            children={0}
-            onDestinationChange = { value => console.log(`destination: ${value}`)}
-            onCheckInChange = { value => console.log(`check-in: ${new Date(value)}`)}
-            onCheckOutChange = { value => console.log(`check-in: ${new Date(value)}`)}
-            onAdultsChange = { value => console.log(`adults: ${value}`)}
-            onChildrenChange = { value => console.log(`children: ${value}`)}
-            onRoomsChange = { value => console.log(`rooms: ${value}`)}
-            onSearch = { () => console.log('Search propeties!')}
-        />
-    ))
-
-    .add('search panel with empty values', () => (
-        <SearchComponent
-            view='panel'
-            onDestinationChange = { value => console.log(`destination: ${value}`)}
-            onCheckInChange = { value => console.log(`check-in: ${new Date(value)}`)}
-            onCheckOutChange = { value => console.log(`check-in: ${new Date(value)}`)}
-            onAdultsChange = { value => console.log(`adults: ${value}`)}
-            onChildrenChange = { value => console.log(`children: ${value}`)}
-            onRoomsChange = { value => console.log(`rooms: ${value}`)}
-            onSearch = { () => console.log('Search propeties!')}
-        />
-    ));
