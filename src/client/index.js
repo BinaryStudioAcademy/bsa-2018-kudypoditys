0a28c18872b2fc878f8acc3a11d18f75d642ab17
igorjv123 <igorjv123@gmail.com>
2018-08-07 20:09:04 +0300

    Fixed component in render

--- src/client/index.js
+++ src/client/index.js
@@ -10,8 +10,6 @@ import reducer from 'client/logic/reducer';
 import App from 'client/components/app';
 import { increment } from 'client/logic/counter/actions';
 
-import Quickfilter from 'client/components/quick-filter'
-
 const store = createStore(
     reducer,
     composeWithDevTools()
@@ -23,7 +21,7 @@ setInterval(() => {
 
 ReactDOM.render(
     <Provider store={store}>
-        <Quickfilter />
+        <App />
     </Provider>,
     document.getElementById('root')
 );
