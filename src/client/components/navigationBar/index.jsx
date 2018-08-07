f1f04d3028087ba982658e1006ef2882fbfa7f35
Nikolay Datsko <sayber1990@yandex.ru>
2018-08-07 13:26:59 +0300

    Base markup

--- src/client/components/navigationBar/index.jsx
+++ src/client/components/navigationBar/index.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Form } from 'semantic-ui-react';
+import { Menu, Label } from 'semantic-ui-react';
 
 export class NavigationBar extends React.Component {
     render() {
         return (
-            <div>
+            <Menu stackable fluid widths={4}>
+                <Menu.Item
+                    name='info-and-price'
+                    onClick={this.handleItemClick}
+                >
+                    Room info and price
+                </Menu.Item>
 
-            </div>
+                <Menu.Item
+                    name='facilities'
+                    onClick={this.handleItemClick}
+                >
+                    Facilities
+                </Menu.Item>
+
+                <Menu.Item name='good-to-know'
+                    onClick={this.handleItemClick}>
+                    Good to know
+                </Menu.Item>
+
+                <Menu.Item name='reviews'
+                    onClick={this.handleItemClick}>
+                    Guest reviews
+                    <Label color='teal'>1</Label>
+                </Menu.Item>
+            </Menu>
         )
     }
 }
