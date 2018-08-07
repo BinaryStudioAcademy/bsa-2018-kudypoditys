4f58c6e4d3624deb85075cdda3db13ab3fe08ef8
unknown <gekag14@gmail.com>
2018-08-07 13:56:42 +0300

    Fixed path to component scss file

--- src/client/components/registration-component/index.jsx
+++ src/client/components/registration-component/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import 'client/components/registrationComponent/index.scss';
+import 'client/components/registration-component/index.scss';
 import PropTypes from 'prop-types';
 import { Button, Input, Container, Header, Icon, Divider, Label, Form } from 'semantic-ui-react';
 
