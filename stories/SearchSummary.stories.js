f76a40fe15831d4ec01059cf8e8773addc90ecac
Dmytro Kostiuk <dmytro.kostiuk@binary-studio.com>
2018-08-08 11:00:00 +0300

    Add stories for SearchSummary component.

---
+++ stories/SearchSummary.stories.js
@@ -0,0 +1,10 @@
+import {storiesOf} from "@storybook/react";
+import React from "react";
+import { SearchSummary } from '../src/client/components/search-summary';
+
+
+storiesOf('SearchSummary component', module)
+
+    .add('Lviv and proper number', () => <SearchSummary data={{destination: "Lviv", numberOfMatched: '12345'}}/>)
+    .add('negative number', () => <SearchSummary data={{destination: "Dnipro", numberOfMatched: '-13321'}}/>)
+    .add('Odessa and invalid number', () => <SearchSummary data={{destination: "Odessa", numberOfMatched: 'sad'}}/>);
