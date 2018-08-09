e355026177b9a3efb9fa19f4e0e3c9fd8d18bd37
fediuchkoelvira <fediuchko.elvira@gmail.com>
2018-08-09 15:08:47 +0300

    Added action and reducer

--- src/client/logic/property-list-item/actions.js
+++ src/client/logic/property-list-item/actions.js
@@ -1,10 +1,38 @@
 import {
-    PROPERTY_LIST_ITEM_UPDATE
+    PROPERTY_ITEM_UPDATE ,
+    PROPERTY_ITEM_DELETE,
+    PROPERTY_ITEM_INSERT ,
+    FETCH_PROPERTY_ITEM,
+    FETCH_ALL_PROPERTY
 } from './actionTypes';
 
-export function propertyListItemUpdate(payload) {
+ export function propertyItemUpdate(payload) {
     return {
-        type: PROPERTY_LIST_ITEM_UPDATE,
-        payload
-    };
-}
+        type: PROPERTY_ITEM_UPDATE,
+         payload
+  };
+};
+export function propertyItemInsert(payload) {
+    return {
+        type: PROPERTY_ITEM_INSERT,
+         payload
+  };
+};
+export function propertyItemDelete(id) {
+    return {
+        type: PROPERTY_ITEM_DELETE,
+         payload : {id}
+  };
+};
+export function fetchPropertyItem(id) {
+    return {
+        type: FETCH_PROPERTY_ITEM,
+         payload : {id},
+  };
+};
+export function fetchAllProperty() {
+    return {
+        type:FETCH_ALL_PROPERTY
+  };
+};
+
--- src/client/logic/property-list-item/reducer.js
+++ src/client/logic/property-list-item/reducer.js
@@ -1,38 +1,51 @@
 import defaultState from 'client/logic/defaultState';
-import {RANKING_BAR_UPDATE} from './actionTypes';
+import {PROPERTY_ITEM_DELETE,
+    PROPERTY_ITEM_INSERT,
+    PROPERTY_ITEM_UPDATE,
+    FETCH_PROPERTY_ITEM,
+    FETCH_ALL_PROPERTY} from './actionTypes';
 
-function sortTypeReducer(state = defaultState.sortType, action) {
+function foundPropertiesReducer(state = defaultState.sortType, action) {
     switch (action.type) {
-        case RANKING_BAR_UPDATE: {
-            return {
-                ...state.sortType,
-                ...action.payload
-            };
+         case PROPERTY_ITEM_DELETE : {
+            const newState = { ...state };
+            delete newState.foundProperties[action._id]
+           return newState;
         }
-        // case COLLECTION_DELETE: {
-        //     const newState = { ...state };
-        //     delete newState.foundProperties[action._id]
-        //     return newState;
-        // }
-        // case COLLECTION_INSERT: {
-        //     return {
-        //         ...state.foundProperties,
-        //         [action._id]: action.payload
-        //     };
-        // }
-        // case COLLECTION_UPDATE: {
-        //     return {
-        //         ...state.foundProperties,
-        //         [action._id]: {
-        //             ...state.foundProperties[action._id],
-        //             ...action.payload
-        //         }
-        //     };
-        // }
+        case PROPERTY_ITEM_INSERT : {
+            return {
+              ...state.foundProperties,
+                [action._id]: action.payload
+           };
+        }
+       case  PROPERTY_ITEM_UPDATE: {
+        return {
+        ...state.foundProperties,
+                [action._id]: {
+                 ...state.foundProperties[action._id],
+                    ...action.payload
+            }
+         };
+     }
+       case FETCH_PROPERTY_ITEM: {
+        return {
+        ...state.foundProperties,
+                [action._id]: {
+                 ...state.foundProperties[action._id],
+                    ...action.payload
+            }
+         };
+     }
+       case FETCH_ALL_PROPERTY: {
+        return {
+        ...state.foundProperties,
+                [action._id]: action.payload
+         };
+     }
         default: {
             return state;
         }
     }
 }
 
-export default sortTypeReducer;
+export default foundPropertiesReducer;
