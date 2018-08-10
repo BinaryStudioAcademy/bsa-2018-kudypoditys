888d859ed504eea0eddbb658660e5b0df3d4ffda
dmitry-skrypka <dimitry.skripka@gmail.com>
2018-08-10 12:24:19 +0300

    added reducer

--- src/client/logic/property-page/reducer.js
+++ src/client/logic/property-page/reducer.js
@@ -1,20 +1,9 @@
 import defaultState from 'client/logic/defaultState';
-import {PROPERTY_DESCRIPTION_UPDATE} from "/logic/property-description/actionTypes";
 
 
 function propertyPageReducer(state = defaultState.shownProperties, action) {
     switch (action.type) {
-        case PROPERTY_DESCRIPTION_UPDATE: {
 
-
-            return {
-                ...state.shownProperties,
-                [action.payload.id]: {
-                    ...action.payload.data
-                }
-
-            };
-        }
         default: {
             return state;
         }
