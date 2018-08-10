e2660e66d3965982341144cd67524a0bf7272eb6
Vladyslav Sorochynskyi <vladyslav.sorochynskyi@gmail.com>
2018-08-10 19:35:56 +0300

    modal stories

---
+++ stories/Modal.stories.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+import Modal from '../src/client/components/modal';
+
+storiesOf('Modal', module)
+    .add('fit content size modal', () => (
+        <Modal
+            trigger={<button>Show modal</button>}
+            >
+            <h1>Hello modal!!</h1>
+        </Modal>
+    ))
+
+    .add('fullscreen size modal', () => (
+        <Modal
+            size='fullScreen'
+            trigger={<button>Show modal</button>}
+            >
+            <h1>Hello fullscreen modal with close icon!!</h1>
+        </Modal>
+    ));
