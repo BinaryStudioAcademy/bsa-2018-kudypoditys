b91e72e22e5267517cde356956a798bb8f7b9f55
dmitry-skrypka <dimitry.skripka@gmail.com>
2018-08-10 18:02:06 +0300

    added User basic route

--- src/api/routes/userRoutes.js
+++ src/api/routes/userRoutes.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const user = express.Router();
-const userService = require('../services/property');
+const userService = require('../services/user');
 
 user.route('/')
     .get((req, res) => {
-
+        userService.getAllUsers()
+            .then(users => {
+                res.send(users);
+            })
+            .catch(err => {
+                console.log(err);
+            });
     })
     .post((req, res) => {
-
+        userService.addUser(req.body)
+            .then(user => {
+                res.send(user);
+            })
+            .catch(err => {
+                console.log(err);
+            });
     });
+
 
 
 user.route('/:id')
