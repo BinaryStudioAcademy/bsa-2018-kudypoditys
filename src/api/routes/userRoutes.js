e6117240475c61bfe8d3ad72ce4cfba131916c57
dmitry-skrypka <dimitry.skripka@gmail.com>
2018-08-10 18:09:00 +0300

    added User/:id basic route

--- src/api/routes/userRoutes.js
+++ src/api/routes/userRoutes.js
@@ -26,12 +26,30 @@ user.route('/')
 
 user.route('/:id')
     .put((req, res) => {
-
+        userService.updateUser(req.params.id, req.body)
+            .then(user => {
+                res.send(user);
+            })
+            .catch(err => {
+                console.log(err);
+            });
     })
     .get((req, res) => {
-
+        userService.getUserById(req.params.id)
+            .then(user => {
+                res.send(user);
+            })
+            .catch(err => {
+                console.log(err);
+            });
     })
     .delete((req, res) => {
+        userService.deleteUser(req.params.id)
+            .then(user => {
+                res.send(user);
+            })
+            .catch(err => {
+                console.log(err);
+            });
 
     });
-
