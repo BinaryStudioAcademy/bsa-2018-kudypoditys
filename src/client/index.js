293a8e69515abeb94d662127f94ff66319aba27d
Vladyslav Sorochynskyi <vladyslav.sorochynskyi@gmail.com>
2018-08-10 15:46:04 +0300

    add client router

--- src/client/index.js
+++ src/client/index.js
@@ -12,25 +12,23 @@ import Search from 'client/components/search';
 import AvailabilityPanel from 'client/components/availability-panel';
 import RankingBar from 'client/components/ranking-bar';
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 const store = createStore(
     reducer,
     composeWithDevTools()
 );
 
+// todo add corresponding pages to routes components
 ReactDOM.render(
     <Provider store={store}>
-        <React.Fragment>
-            {[
-                <Search
-                    key="Search"
-                    view='bar'
-                />,
-                <AvailabilityPanel
-                    key='Availability'
-                />
-            ]}
-        </React.Fragment>
-
+        <BrowserRouter>
+        <Switch>
+            <Route exact path='/' component={Search}/>
+            <Route path='/search' component={Search}/>
+            <Route path='/property/:id' component={Search}/>
+        </Switch>
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root')
 );
