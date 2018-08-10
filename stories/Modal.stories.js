b6b17385303c2c7c71b5ed2944d4d6537f47d8a1
Vladyslav Sorochynskyi <vladyslav.sorochynskyi@gmail.com>
2018-08-10 19:42:29 +0300

    added modal stories with headers

--- stories/Modal.stories.js
+++ stories/Modal.stories.js
@@ -13,8 +13,27 @@ storiesOf('Modal', module)
         </Modal>
     ))
 
+    .add('fit content size modal with header', () => (
+        <Modal
+            header='Hotel Dolynskyi'
+            trigger={<button>Show modal</button>}
+            >
+            <h1>Hello modal!!</h1>
+        </Modal>
+    ))
+
     .add('fullscreen size modal', () => (
         <Modal
+            size='fullScreen'
+            trigger={<button>Show modal</button>}
+            >
+            <h1>Hello fullscreen modal with close icon!!</h1>
+        </Modal>
+    ))
+
+    .add('fullscreen size modal with header', () => (
+        <Modal
+            header='Hotel Dolynskyi'
             size='fullScreen'
             trigger={<button>Show modal</button>}
             >
