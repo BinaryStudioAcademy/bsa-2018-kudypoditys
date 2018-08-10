1f308b17a62006db03a9813973944d61ab1330ef
dmitry-skrypka <dimitry.skripka@gmail.com>
2018-08-10 18:23:14 +0300

    added property/:id basic route, service

--- src/api/routes/propertyRoutes.js
+++ src/api/routes/propertyRoutes.js
@@ -4,22 +4,52 @@ const propertyService = require('../services/property');
 
 property.route('/')
     .get((req, res) => {
-
-
+        propertyService.getAllproperties()
+            .then(property => {
+                res.send(property);
+            })
+            .catch(err => {
+                console.log(err);
+            });
     })
     .post((req, res) => {
-
+        propertyService.addProperty(req.body)
+            .then(property => {
+                res.send(property);
+            })
+            .catch(err => {
+                console.log(err);
+            });
     });
 
 
 property.route('/:id')
     .put((req, res) => {
-
+        propertyService.updateProperty(req.params.id, req.body)
+            .then(property => {
+                res.send(property);
+            })
+            .catch(err => {
+                console.log(err);
+            });
     })
     .get((req, res) => {
-
+        propertyService.getPropertyById(req.params.id)
+            .then(property => {
+                res.send(property);
+            })
+            .catch(err => {
+                console.log(err);
+            });
     })
     .delete((req, res) => {
-
+        propertyService.deleteProperty(req.params.id)
+            .then(property => {
+                res.send(property);
+            })
+            .catch(err => {
+                console.log(err);
+            });
     });
 
+module.exports = property;
--- src/api/services/property.js
+++ src/api/services/property.js
@@ -2,7 +2,26 @@ const Service = require('./generalService');
 const propertyRepository = require("../repositories/propertyRepository");
 
 class PropertyService extends Service {
-    // todo add service logic
+    getAllproperties() {
+        return propertyRepository.findAll();
+    }
+
+    getPropertyById(id) {
+        return propertyRepository.findById(id);
+    }
+
+    addProperty(property) {
+        return propertyRepository.create(property);
+    }
+
+    updateProperty(id, property) {
+        return propertyRepository.updateById({_id: id}, property);
+    }
+
+    deleteProperty(id) {
+        return propertyRepository.deleteById({_id: id});
+    }
 }
+
 
 module.exports = new PropertyService(propertyRepository);
