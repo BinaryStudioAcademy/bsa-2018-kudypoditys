fa88daf7ab7410fa0160ff476d750e63648ab716
Vladyslav Sorochynskyi <vladyslav.sorochynskyi@gmail.com>
2018-08-13 15:49:47 +0300

    registration

--- src/api/routes/authRoutes.js
+++ src/api/routes/authRoutes.js
@@ -1,26 +1,60 @@
-const express = require('express');
+const express = require("express");
 const authRouter = express.Router();
-const userService = require('../services/user');
-const jwtMiddleware = require('../middleware/jwt.middleware');
+const userService = require("../services/user");
+const jwtMiddleware = require("../middleware/jwt.middleware");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-authRouter.route('/login')
+authRouter
+    .route("/login")
     .post((req, res) => {
-        const data = req.body;
-        userService.login(data.email, data.password)
-            .then((token) => {
-                res.cookie('jwtToken', token).status(200).send(true);
+        const data = req.data;
+        userService
+            .login(data.email, data.password)
+            .then(token => {
+                res.cookie("jwtToken", token)
+                    .status(200)
+                    .send(true);
             })
             .catch(err => {
                 res.status(400).send(err.message);
             });
-
     })
     .get(jwtMiddleware);
 
-authRouter.route('/logout')
-    .get((req, res) => {
-        res.clearCookie('jwtToken').send(true);
+authRouter.route("/logout").get((req, res) => {
+    res.clearCookie("jwtToken").send(true);
+});
 
-    });
+authRouter.route("/signup").post((req, res) => {
+    const data = req.body;
+    const hash = bcrypt.hashSync(data.password.trim(), 10);
+    const user = {
+        fullName: data.name.trim(),
+        password: hash,
+        email: data.email.trim(),
+        phoneNumber: data.phoneNumber.trim(),
+        avatar: data.avatar.trim()
+    };
+    userService
+        .addUser(user)
+        .then(() => {
+            userService.getUserByEmail(user.email).then(user => {
+                const token = jwt.sign(
+                    {
+                        id: user.id,
+                        fullName: user.fullName
+                    },
+                    "My secret"
+                );
+                res.cookie("jwtToken", token)
+                    .status(200)
+                    .send(true);
+            });
+        })
+        .catch(err => {
+            res.status(400).send(err.message);
+        });
+});
 
 module.exports = authRouter;
