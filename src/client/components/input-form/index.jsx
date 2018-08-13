809e865ef250559e92758ba229496226a2be3727
fediuchkoelvira <fediuchko.elvira@gmail.com>
2018-08-13 16:40:07 +0300

    Fixed validate

--- src/client/components/input-form/index.jsx
+++ src/client/components/input-form/index.jsx
@@ -2,32 +2,38 @@ import React from 'react';
 import { reduxForm ,Field} from 'redux-form';
 import { Input} from 'semantic-ui-react';
 
+const required = value => (value || typeof value === 'number' ? undefined : 'Required')
 
-const validate = values => {
-    const errors = {}
-    if (!values.customName) {
-        errors.customName = 'Required'
-    } else if (values.customName.length > 15) {
-        errors.customName = 'Must be 15 characters or less'
-    }
-    if (!values.email) {
-        errors.email = 'Required'
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = 'Invalid email address'
-    }
-    if (!values.propetyName) {
-        errors.propetyName = 'Required'
-    } else if (values.propetyName.length > 25) {
-        errors.propetyName = 'Must be 25 characters or less'
-    }
-    if (!values.phoneNumber) {
-        errors.phoneNumber = 'Required'
-    } else if (!/^(0|[1-9][0-9]{9})$/i.test(values.phoneNumber)) {
-        errors.phoneNumber = 'Invalid phone number, must be 10 digits'
+const maxLength = max => value =>
+  value && value.length > max ? `Must be ${max} characters or less` : undefined
 
-        return errors
-    }
-}
+const maxLength20 = maxLength(20)
+
+const minLength = min => value =>
+
+  value && value.length < min ? `Must be ${min} characters or more` : undefined
+const minLength2 = minLength(2)
+
+const email = value =>
+  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
+    ? 'Invalid email address'
+    : undefined
+
+ const phoneNumber = value =>
+  value && !/^(0|[1-9][0-9]{9})$/i.test(value)
+    ? 'Invalid phone number, must be 10 digits'
+    : undefined
+
+const alphaNumeric = value =>
+  value && /[^a-zA-Z0-9 ]/i.test(value)
+    ? 'Only alphanumeric characters'
+    : undefined
+
+const aol = value =>
+  value && /.+@aol\.com/.test(value)
+    ? 'Really? You still use AOL for your email?'
+    : undefined
+
 const warn = values => {
     const warnings = {}
     if (values.propertyname > 20) {
@@ -50,15 +56,13 @@ const InputForm = (props) => {
     const { handleSubmit } = props
     return (
         <form onSubmit={handleSubmit}>
-        <Field
+      <Field
+        name="username"
+        type="text"
         component={renderField}
-        name="property name"
-        type="PropertyName"
-        label="Name"
-        placeholder="Property Name"
-        required="required"
-        className="form-control"
-
+        label="Username"
+        validate={[required, maxLength20, minLength2]}
+        warn={alphaNumeric}
       />
         </form>
     )
@@ -67,6 +71,5 @@ const InputForm = (props) => {
 
 export default reduxForm({
     form: 'InputForm',
-    validate,
     warn
 })(InputForm)
