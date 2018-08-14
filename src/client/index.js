46a1e1c2ae86ab11b1927261460086f9b8d5cac1
fediuchkoelvira <fediuchko.elvira@gmail.com>
2018-08-14 16:36:57 +0300

    fixed index.js

--- src/client/index.js
+++ src/client/index.js
@@ -8,23 +8,23 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import "client/styles/global.scss";
 import reducer from "client/logic/reducer";
 
-// import Search from "client/components/search";
+import Search from "client/components/search";
 
-//  import LoginComponent from "./components/login";
+ import LoginComponent from "./components/login";
 
-//  import SearchSummary from "client/components/search-summary";
-//  import AvailabilityPanel from "client/components/availability-panel";
-//  import RankingBar from "client/components/ranking-bar";
-//  import PropertyDescription from "client/components/property-description";
-//  import PropertyListItem from "client/components/property-list-item";
-// import {PropertyCreationTabs} from 'client/components/property-creation-tabs'
+ import SearchSummary from "client/components/search-summary";
+  import AvailabilityPanel from "client/components/availability-panel";
+ import RankingBar from "client/components/ranking-bar";
+ import PropertyDescription from "client/components/property-description";
+ import PropertyListItem from "client/components/property-list-item";
+ import {PropertyCreationTabs} from 'client/components/property-creation-tabs'
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-// import InputForm from "./components/input-form";
-// import { Registration } from './components/registration';
+import InputForm from "./components/input-form";
+import { Registration } from './components/registration';
 import RoomItem from './components/room-item';
 
-// import { DummyComponent } from "./helpers/dummyComponent";
-// import BasicMapWidget from './components/basic-map-widget';
+import { DummyComponent } from "./helpers/dummyComponent";
+ import BasicMapWidget from './components/basic-map-widget';
 
 
 const store = createStore(reducer, composeWithDevTools());
@@ -36,9 +36,9 @@ ReactDOM.render(
             <Switch>
             <Route  exact path="/" render={()=><RoomItem roomData = { {title:"double room", amount:2} }/>}/>
 
-                 {/* <Route path="/search" component={Registration} />
+                  <Route path="/search" component={Registration} />
                 <Route path="/property/:id" component={Registration} />
-                <Route path="/add-property/" component={PropertyCreationTabs} />  */}
+                <Route path="/add-property/" component={PropertyCreationTabs} />
 
             </Switch>
         </BrowserRouter>
