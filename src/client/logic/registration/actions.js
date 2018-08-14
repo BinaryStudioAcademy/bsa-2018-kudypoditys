75764619e5aa54db183a8149e94798416a3c7f55
Vladyslav Sorochynskyi <vladyslav.sorochynskyi@gmail.com>
2018-08-14 20:00:40 +0300

    skeleton for signup handle

--- src/client/logic/registration/actions.js
+++ src/client/logic/registration/actions.js
@@ -1,8 +1,16 @@
 import { REGISTER_SUCCESS } from "./actionTypes";
+import api from "../.././helpers/api";
 
 export function registerSubmit(payload) {
     // Saga
-    console.log(JSON.stringify(payload));
+    api.sendRequest("/signup", "post", payload)
+        .then(response => {
+            console.log("DATA: " + JSON.stringify(response.data));
+        })
+        .catch(err => {
+            console.log(JSON.stringify(err.data));
+        });
+
     return {
         type: REGISTER_SUCCESS
     };
