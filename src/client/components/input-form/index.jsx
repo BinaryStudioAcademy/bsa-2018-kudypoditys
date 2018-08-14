1fa6898169e224694938c69f3084013c07c056f5
fediuchkoelvira <fediuchko.elvira@gmail.com>
2018-08-14 11:44:20 +0300

    Fixed PR comment

--- src/client/components/input-form/index.jsx
+++ src/client/components/input-form/index.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
-import { reduxForm, Field } from 'redux-form';
-import FormTextInput from './formTextInput';
+import { reduxForm } from 'redux-form';
 import { required, phoneNumber } from './../../regexValidationService'
+import './index.scss'
+import { Input } from 'semantic-ui-react';
 
-
+ export const FormTextInput = ({ input, max, min, label, type, required, autocomplete, meta: { touched, error } }) => {
+    return (
+    <div className="input">
+        <input {...input}
+            type={type}
+            placeholder={label}
+            required={required}
+            autoComplete={autocomplete}
+            max={max}
+            min={min} />
+        {touched &&
+                error &&
+                <span className='error'>
+                    {error}
+                </span>}
+    </div>
+)
+}
 
 const InputForm = (props) => {
     return (
-        < Field
+        <Input
             name="phone"
             type="number"
             label="Phone number"
             validate={[required, phoneNumber]}
             component={FormTextInput}
-        />
+            />
+
     )
 };
 
