d4262b9364f343f4db44ddc18b4e73185f657d2f
igorjv123 <igorjv123@gmail.com>
2018-08-14 12:49:01 +0300

    Fixed index.js

--- src/client/index.js
+++ src/client/index.js
@@ -8,24 +8,23 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import "client/styles/global.scss";
 import reducer from "client/logic/reducer";
 
-// import Slider from "client/components/slider";
-// import Registration from "client/components/registration";
-// import { PropertyCreationTabs } from "client/components/property-creation-tabs";
-// import { BrowserRouter, Route, Switch } from "react-router-dom";
-// // import CheckInCheckOut from 'client/components/checkin-checkout'
-// import {HomePage} from 'client/components/home-page'
-// import PropertyPage from "client/components/property-page";
-// import Search from "client/components/search";
-// import Login from "client/components/login";
-// import SearchPage from "client/components/search-page";
-// import {NotFoundPage} from "client/components/404-page";
-import Breadcrumbs from "client/components/breadcrumbs";
+import Slider from "client/components/slider";
+import Registration from "client/components/registration";
+import { PropertyCreationTabs } from "client/components/property-creation-tabs";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+// import CheckInCheckOut from 'client/components/checkin-checkout'
+import {HomePage} from 'client/components/home-page'
+import PropertyPage from "client/components/property-page";
+import Search from "client/components/search";
+import Login from "client/components/login";
+import SearchPage from "client/components/search-page";
+import {NotFoundPage} from "client/components/404-page";
 
 const store = createStore(reducer, composeWithDevTools());
 
 ReactDOM.render(
     <Provider store={store}>
-        {/* <BrowserRouter>
+        <BrowserRouter>
             <Switch>
                 <Route exact path="/" component={HomePage}/>
                 <Route exact path="/reg" component={Registration}/>
@@ -36,8 +35,7 @@ ReactDOM.render(
                 <Route path="/404" component={NotFoundPage}/>
 
             </Switch>
-        </BrowserRouter> */}
-        <Breadcrumbs />
+        </BrowserRouter>
     </Provider>,
     document.getElementById("root")
 );
