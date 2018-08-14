de09048b9ee92cf82640e5849a543291351a3e93
Nikolay Datsko <sayber1990@yandex.ru>
2018-08-14 21:42:51 +0300

    Implemented basic logic

--- src/client/components/phone-input/index.jsx
+++ src/client/components/phone-input/index.jsx
@@ -3,25 +3,39 @@ import { Dropdown, Input } from "semantic-ui-react";
 import countries from "./countriesInfo.js";
 import "./index.scss";
 
-const PhoneInput = () => (
-    <div>
-        <Input
-            action={
-                <Dropdown
-                    className="phone-dropdown"
-                    compact={true}
-                    left="true"
-                    button
-                    basic
-                    floating
-                    selection
-                    defaultValue="+380"
-                    options={countries}
-                />
-            }
-            actionPosition={"left"}
-            placeholder="Phone"
-        />
-    </div>
-);
+class PhoneInput extends React.Component {
+    handleNumberChanged = (event, value) => {
+        this.props.handleNumberChanged(value.value);
+    };
+
+    handlePrefixChanged = (event, value) => {
+        this.props.handlePrefixChanged(value.value);
+    };
+
+    render() {
+        return (
+            <Input
+                action={
+                    <Dropdown
+                        className="phone-dropdown"
+                        compact={true}
+                        left="true"
+                        button
+                        basic
+                        floating
+                        selection
+                        defaultValue="+380"
+                        options={countries}
+                        onChange={this.handlePrefixChanged}
+                    />
+                }
+                actionPosition={"left"}
+                placeholder="Phone"
+                onChange={this.handleNumberChanged}
+                className={this.props.error ? "error" : ""}
+            />
+        );
+    }
+}
+
 export default PhoneInput;
--- stories/PhoneInput.stories.js
+++ stories/PhoneInput.stories.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
 import PhoneInput from "../src/client/components/phone-input";
 
-storiesOf("Phone input", module).add("Basic", () => <PhoneInput />);
+const handleNumberChanged = value => {
+    console.log("Number changed", value);
+};
+
+const handlePrefixChanged = value => {
+    console.log("Prefix changed", value);
+};
+
+storiesOf("Phone input", module)
+    .add("Basic", () => (
+        <PhoneInput
+            handleNumberChanged={handleNumberChanged}
+            handlePrefixChanged={handlePrefixChanged}
+        />
+    ))
+    .add("Error", () => (
+        <PhoneInput
+            error={true}
+            handleNumberChanged={handleNumberChanged}
+            handlePrefixChanged={handlePrefixChanged}
+        />
+    ));
