10e0b001195ad03cb1046178645d7e847f3e2e60
Bohdan Rusinka <bohdan.rusinka@binary-studio.com>
2018-08-14 09:07:28 +0300

    Updated api routes, make server work in production; fixed app launch on ubuntu

--- server.js
+++ server.js
@@ -1,29 +1,33 @@
-const
-    path = require('path'),
-    express = require('express'),
-    cors = require('cors'),
-    bodyParser = require('body-parser'),
-    compression = require('compression');
+const path = require("path"),
+    express = require("express"),
+    cors = require("cors"),
+    bodyParser = require("body-parser"),
+    compression = require("compression");
 // routes = require(`${apiRoot}/testModels`);
 
+const apiRoot = path.resolve(path.join(__dirname, "src/api"));
 
-const apiRoot = path.resolve(path.join(__dirname, 'src/api'));
-
-const
-    app = express(),
+const app = express(),
     port = process.env.PORT || 5000;
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
+const routes = require("./src/api/routes")(app);
 
-const routes = require('./src/api/routes')(app);
+app.get("/*", (req, res) => {
+    const fileDirectory = path.join(__dirname, "public");
+
+    res.sendFile("index.html", { root: fileDirectory }, err => {
+        res.end();
+    });
+});
 
 app.listen(port, () => {
-    console.log('Server running on http://127.0.0.1:%s', port);
+    console.log("Server running on http://127.0.0.1:%s", port);
 });
 
 require(`${apiRoot}/testModels`)();
