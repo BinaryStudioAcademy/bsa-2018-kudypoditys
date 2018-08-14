7bbdee5fccc2f0919268b633581404f0cbd7be78
didukhn <nata737mail@gmail.com>
2018-08-14 11:33:43 +0300

    unintall uuid

--- src/api/services/userToken.js
+++ src/api/services/userToken.js
@@ -2,8 +2,6 @@ const Service = require("./generalService");
 const userRefreshTokenRepository = require("../repositories/userRefreshTokenRepository");
 const jwt = require('jsonwebtoken');
 const settings = require('../../../config/settings');
-const bcrypt = require('bcrypt');
-const uuidv1 = require('uuid/v1');
 const { dateHelpers } = require('../helpers');
 
 class UserTokenService extends Service {
@@ -40,23 +38,23 @@ class UserTokenService extends Service {
         return secondsFromUnixEpoch + settings.accessTokenLife;
     }
 
-    refreshToken(token) {
+    refreshToken(oldRefreshToken) {
         let userId;
         try {
-            userId = jwt.verify(token, settings.jwtRefreshTokenPrivateKey).userId;
+            userId = jwt.verify(oldRefreshToken, settings.jwtRefreshTokenPrivateKey).userId;
         } catch (err) {
             return Promise.reject(new Error('invalid refresh token'));
         }
 
         return this.getByUserId(userId).then(model => {
-            if (!model) {
+            if (!model) {//if user singup but not login
                 return Promise.reject(new Error('refresh token not found'));
             }
             const { refreshToken } = model.dataValues;
             const { tillDate } = model.dataValues;
             const currDate = dateHelpers.toUnixTimeSeconds(new Date());
 
-            if (refreshToken !== token) {
+            if (refreshToken !== oldRefreshToken) {
                 return Promise.reject(new Error('unknown refresh token for this user'));
             }
             if (tillDate < currDate) {
