b06d2531f09eb812157e8eb0cdd5f5391d11310d
Vladyslav Sorochynskyi <vladyslav.sorochynskyi@gmail.com>
2018-08-15 10:32:12 +0300

    set cookies in signup

--- src/client/logic/registration/actions.js
+++ src/client/logic/registration/actions.js
@@ -4,17 +4,19 @@ import {
     REGISTER_FAILURE
 } from "./actionTypes";
 import api from "../.././helpers/api";
+import cookies from "browser-cookies";
 
 export function registerSubmit(payload) {
     // Saga
     api.sendRequest("/signup", "post", payload)
         .then(response => {
+            const { token, expiresIn } = response.data;
+            cookies.set("accessToken", token, { expires: new Date(expiresIn) });
             return {
                 type: REGISTER_SUCCESS,
                 payload: {
                     error: false,
-                    message: "Successfully signed up!",
-                    jwtToken: response.data
+                    message: "Successfully signed up!"
                 }
             };
         })
