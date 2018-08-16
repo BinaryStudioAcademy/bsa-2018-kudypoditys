0ae5d028045bb6b9b098213515db5fbbb494476c
fediuchkoelvira <fediuchko.elvira@gmail.com>
2018-08-16 20:38:14 +0300

    Added container

--- src/client/logic/main-info-tab/action.js
+++ src/client/logic/main-info-tab/action.js
@@ -1,6 +1,6 @@
 import { ADD_POPERTY } from "./actionTypes";
 
-export function PropertySubmit(payload) {
+export function propertySubmit(payload) {
     return {
         type: ADD_POPERTY,
         payload
