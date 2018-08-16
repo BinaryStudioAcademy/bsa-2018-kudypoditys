385d3381ad0bd4ba5caa42dde02c6787e23e7f39
dmitry-skrypka <dimitry.skripka@gmail.com>
2018-08-16 12:17:09 +0300

    added semantic selector w/ redux-form

---
+++ src/client/components/dropdown-form/semanticSelectorForm.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Form, Input} from 'semantic-ui-react';
+
+
+export default function semanticSelectorFormField({input, type, label, placeholder, meta: {touched, error, warning}, as: As = Input, ...props}) {
+    function handleChange(e, {value}) {
+        return input.onChange(value);
+    }
+
+    return (
+        <Form.Field>
+            <As {...props} {...input} value={input.value} type={type} label={label} placeholder={placeholder}
+                onChange={handleChange}/>
+
+        </Form.Field>
+    );
+}
+
+semanticSelectorFormField.propTypes = {
+    as: PropTypes.any,
+    input: PropTypes.object,
+    type: PropTypes.string,
+    label: PropTypes.string,
+    placeholder: PropTypes.string,
+    meta: PropTypes.object
+};
+
