c57213f1b65219c9bedba62c4fa8c67a9e82eb6e
unknown <gekag14@gmail.com>
2018-08-16 18:11:13 +0300

    Changed map from gmap to mapbox-gl

--- src/client/components/map-widget-modal/index.jsx
+++ src/client/components/map-widget-modal/index.jsx
@@ -1,32 +1,31 @@
 import React, { Component } from 'react';
 import "./index.scss";
 
-import Map from 'client/components/basic-map-widget/map';
 import { Button, Modal } from 'semantic-ui-react';
+import MapView from "../map-view";
 
 export default class MapWidgetModal extends Component {
     static defaultProps = {
-        gmapLocation: { lat: -34.397, lng: 150.644 },
-        gmap_url: `https://maps.googleapis.com/maps/api/js?key=AIzaSyA0dkzMEoW2UBi7tA1TVAMbrwCUStF_9xw&v=3.exp&libraries=geometry,drawing,places`
+        latitude: -34.397,
+        longitude: 150.644,
+        buttonText: "ShowOnMap"
     };
 
     render() {
-        console.log(this.props.gmapLocation);
         return (
             <React.Fragment>
                 <Modal
-                    trigger={<Button basic size='tiny' className='map-widget-modal-button'>Show On Map</Button>}
+                    trigger={<Button basic size='tiny' className={this.props.buttonClass}>{this.props.buttonText}</Button>}
                     closeIcon
                     className='map-widget-modal'
                 >
                     <Modal.Content>
-                        <Map
-                            location={this.props.gmapLocation}
-                            googleMapURL={this.props.gmap_url}
-                            loadingElement={<div style={{ height: `100%` }} />}
-                            containerElement={<div style={{ height: '100%', width: '100%' }} />}
-                            mapElement={<div style={{ height: `100%` }} />}
-                        />
+                            <MapView
+                                latitude={this.props.latitude}
+                                longitude={this.props.longitude}
+                                zoom={12}
+                                controlEnable={true}
+                            />
                     </Modal.Content>
 
                 </Modal>
