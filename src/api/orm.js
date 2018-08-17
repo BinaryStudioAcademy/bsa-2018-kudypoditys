fb7aa89d77f7c65a348ead4016c6116d22bc4266
dmitry-skrypka <dimitry.skripka@gmail.com>
2018-08-17 14:46:05 +0300

    added HOST, PORT

--- src/api/orm.js
+++ src/api/orm.js
@@ -5,14 +5,19 @@ const
     DB_NAME = process.env.DB_NAME,
     DB_USER = process.env.DB_USER,
     DB_PASS = process.env.DB_PASS,
-    DB_DIALECT = {dialect: process.env.DIALECT};
+    DB_OPTIONS = {
+        host: process.env.HOST,
+        port: process.env.PORT,
+        dialect: process.env.DIALECT
+    };
+
 
 //Change values above to your OWN (.env file)
 const orm = new Sequelize(
     DB_NAME,
     DB_USER,
     DB_PASS,
-    DB_DIALECT
+    DB_OPTIONS
 );
 
 
