ba42d8a40d6a0330c19d1cc9e0b0aa35a0cea374
didukhn <nata737mail@gmail.com>
2018-08-17 22:15:26 +0300

    created page component

---
+++ src/client/components/page/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component, Fragment } from 'react';
+import './index.scss';
+import Header from 'client/components/header';
+
+class Page extends Component {
+
+    render() {
+        return (<Fragment>
+            <Header></Header>
+            {this.props.children}
+            <footer>
+                Maybe till to the end of the academy we will make a footer, but now we have this...
+            </footer>
+        </Fragment>);
+    }
+}
+
+export default Page;
