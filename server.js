871fc40f75201b55d1a5abf928ab6513c3b15c63
Vladyslav Sorochynskyi <vladyslav.sorochynskyi@gmail.com>
2018-08-17 10:00:15 +0300

    require passport

--- server.js
+++ server.js
@@ -1,35 +1,36 @@
-const
-    path = require('path'),
-    express = require('express'),
-    cors = require('cors'),
-    bodyParser = require('body-parser'),
-    cookieParser = require('cookie-parser'),
+const path = require("path"),
+    express = require("express"),
+    cors = require("cors"),
+    bodyParser = require("body-parser"),
+    cookieParser = require("cookie-parser"),
     compression = require("compression");
 // routes = require(`${apiRoot}/testModels`);
 
 const apiRoot = path.resolve(path.join(__dirname, "src/api"));
 
+require(`${apiRoot}/helpers/passport`);
+
 const app = express(),
     port = process.env.PORT || 5000;
 
-app.use(cors({origin: "http://localhost:3000", credentials: true}));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cookieParser());
-
-require('./src/api/middleware')(app);//adding jwt and other
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 
-require('./src/api/middleware')(app);//adding jwt and other
+require("./src/api/middleware")(app); //adding jwt and other
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+
+require("./src/api/middleware")(app); //adding jwt and other
 const routes = require("./src/api/routes")(app);
 
 app.get("/*", (req, res) => {
     const fileDirectory = path.join(__dirname, "public");
 
-    res.sendFile("index.html", {root: fileDirectory}, err => {
+    res.sendFile("index.html", { root: fileDirectory }, err => {
         res.end();
     });
 });
@@ -38,4 +39,4 @@ app.listen(port, () => {
     console.log("Server running on http://127.0.0.1:%s", port);
 });
 
-require(`${apiRoot}/testModels`)();
+//require(`${apiRoot}/testModels`)();
