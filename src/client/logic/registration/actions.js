e37f7631372e1d8f8bfca3cf198f04a08c03d482
Vladyslav Sorochynskyi <vladyslav.sorochynskyi@gmail.com>
2018-08-18 15:14:14 +0300

    sign up saga

--- src/client/logic/registration/actions.js
+++ src/client/logic/registration/actions.js
@@ -1,33 +1,9 @@
-import {
-    REGISTER_SUBMIT,
-    REGISTER_SUCCESS,
-    REGISTER_FAILURE
-} from "./actionTypes";
-import api from "../.././helpers/api";
-import cookies from "browser-cookies";
+import { REGISTER_SUBMIT } from "./actionTypes";
 
 export function registerSubmit(payload) {
     // Saga
-    api.sendRequest("/signup", "post", payload)
-        .then(response => {
-            const { token, expiresIn } = response.data;
-            cookies.set("accessToken", token);
-            cookies.set("expiresIn", expiresIn.toString());
-            return {
-                type: REGISTER_SUCCESS,
-                payload: {
-                    error: false,
-                    message: "Successfully signed up!"
-                }
-            };
-        })
-        .catch(err => {
-            return {
-                type: REGISTER_FAILURE,
-                payload: {
-                    error: true,
-                    message: err.data
-                }
-            };
-        });
+    return {
+        type: REGISTER_SUBMIT,
+        payload: payload
+    };
 }
