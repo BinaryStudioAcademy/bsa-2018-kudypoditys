2d64475c3d18654af87243436ef1f131eb49e751
Vladyslav Sorochynskyi <vladyslav.sorochynskyi@gmail.com>
2018-08-18 14:42:34 +0300

    signup method in auth service

--- src/client/services/authService.js
+++ src/client/services/authService.js
@@ -1,27 +1,42 @@
-import api from '../helpers/api';
-import cookies from 'browser-cookies';
-import history from 'client/history';
+import api from "../helpers/api";
+import cookies from "../helpers/cookie-tool";
+import history from "client/history";
 
 class AuthService {
+    signup(user) {
+        return api
+            .sendRequest("/api/signup", "post", user)
+            .then(response => {
+                const { accessToken, refreshToken } = response.data;
+                cookies.setTokens(accessToken, refreshToken);
+            })
+            .catch(err => {
+                Promise.reject(new Error(err.message));
+            });
+    }
+
     login(email, password) {
-        return api.sendRequest('/api/login', 'post', {
-            email, password
-        }).then(response => {
-            const { token, expiresIn, refreshToken } = response.data;
-            
-            cookies.set('accessToken', token);
-            cookies.set('expiresIn', expiresIn.toString());
-            cookies.set('refreshToken', refreshToken);
-        }).then(_ => {
-            history.push('/');
-        });
+        return api
+            .sendRequest("/api/login", "post", {
+                email,
+                password
+            })
+            .then(response => {
+                const { token, expiresIn, refreshToken } = response.data;
+
+                cookies.set("accessToken", token);
+                cookies.set("expiresIn", expiresIn.toString());
+                cookies.set("refreshToken", refreshToken);
+            })
+            .then(_ => {
+                history.push("/");
+            });
     }
 
     logout() {
-        cookies.erase('accessToken');
-        cookies.erase('expiresIn');
-        cookies.erase('refreshToken');
-        history.push('/log');
+        cookies.erase("accessToken");
+        cookies.erase("refreshToken");
+        history.push("/log");
     }
 }
 
