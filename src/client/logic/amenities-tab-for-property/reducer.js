236c003455a06a85b5f56336e6cfedd7b1332039
fediuchkoelvira <fediuchko.elvira@gmail.com>
2018-08-19 12:18:50 +0300

    Fixed actions for main-info-tab

--- src/client/logic/amenities-tab-for-property/reducer.js
+++ src/client/logic/amenities-tab-for-property/reducer.js
@@ -1,6 +1,6 @@
 import {AMENITIES_TAB_UPDATE } from "./actionTypes";
 
-export default function amenitiesTabReducer(state, action) {
+export default function amenitiesTabReducer(state = {}, action) {
     switch (action.type) {
         case AMENITIES_TAB_UPDATE: {
             return {
