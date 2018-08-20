acd7c8088a83a8d90d55bd30c6c45e9c1a71f5ad
fediuchkoelvira <fediuchko.elvira@gmail.com>
2018-08-20 19:10:58 +0300

    Fixed property service

--- src/client/services/propertyService.js
+++ src/client/services/propertyService.js
@@ -1,28 +1,15 @@
 import api from '../helpers/api';
-import cookies from 'browser-cookies';
 import history from 'client/history';
 
 class PropertyService {
     createPropery(data) {
         return api.sendRequest('/api/add-property/', 'post',data).then(response => {
-            const { token, expiresIn, refreshToken } = response.data;
-
-            cookies.set('accessToken', token);
-            cookies.set('expiresIn', expiresIn.toString());
-            cookies.set('refreshToken', refreshToken);
-        }).then(_ => {
             history.push('/add-property/');
         });
     }
 
     updatePropery(data) {
         return api.sendRequest(`/api/add-property/${data.propertyId}`, 'put',data).then(response => {
-            const { token, expiresIn, refreshToken } = response.data;
-
-            cookies.set('accessToken', token);
-            cookies.set('expiresIn', expiresIn.toString());
-            cookies.set('refreshToken', refreshToken);
-        }).then(_ => {
             history.push('/add-property/');
         });
     }
