3b6e54bd7d418891e2d94aaf40a937fec24d3748
unknown <gekag14@gmail.com>
2018-08-20 12:05:01 +0300

    Added register saga to rootSaga

--- src/client/logic/rootSaga.js
+++ src/client/logic/rootSaga.js
@@ -1,11 +1,9 @@
-import { all, fork } from 'redux-saga/effects';
+import { all, fork } from "redux-saga/effects";
 
-import loginSaga from 'client/logic/login/saga';
-import headerSaga from 'client/logic/header/saga';
+import loginSaga from "client/logic/login/saga";
+import signupSaga from "client/logic/registration/saga";
+import headerSaga from "client/logic/header/saga";
 
 export default function* rootSaga() {
-    yield all([
-        fork(loginSaga),
-        fork(headerSaga)
-    ])
+    yield all([fork(loginSaga), fork(signupSaga), fork(headerSaga)]);
 }
