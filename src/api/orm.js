600d3dcf004d1d84bc70b26b3ba11ffae1d298d2
Vladyslav Sorochynskyi <vladyslav.sorochynskyi@gmail.com>
2018-08-20 21:32:06 +0300

    separate db host and port

--- src/api/orm.js
+++ src/api/orm.js
@@ -1,26 +1,16 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
-const
-    DB_NAME = process.env.DB_NAME,
+const DB_NAME = process.env.DB_NAME,
     DB_USER = process.env.DB_USER,
     DB_PASS = process.env.DB_PASS,
     DB_OPTIONS = {
-        host: process.env.HOST,
-        port: process.env.PORT,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
         dialect: process.env.DIALECT
     };
 
-
 //Change values above to your OWN (.env file)
-const orm = new Sequelize(
-    DB_NAME,
-    DB_USER,
-    DB_PASS,
-    DB_OPTIONS
-);
-
-
+const orm = new Sequelize(DB_NAME, DB_USER, DB_PASS, DB_OPTIONS);
 
 module.exports = orm;
-
