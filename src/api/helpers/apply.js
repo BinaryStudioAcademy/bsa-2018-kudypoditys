ad2c8e35b3174fa5851bcee1676d7bdeb582a4f7
Vladyslav Sorochynskyi <vladyslav.sorochynskyi@gmail.com>
2018-08-20 21:53:08 +0300

    remove console.log

--- src/api/helpers/apply.js
+++ src/api/helpers/apply.js
@@ -1,8 +1,6 @@
 const apply = (pathes, middleware) => {
     return (req, res, next) => {
         for (let path of pathes) {
-            console.log(JSON.stringify(path));
-            console.log("url path: " + path.url);
             if (
                 path.url.indexOf(req.path) === 0 &&
                 req.method.toLowerCase() === path.method.toLowerCase()
