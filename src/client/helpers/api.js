38a3d60c85edd5ad75bbbe8949e46c62e158eb33
Bohdan Rusinka <bohdan.rusinka@binary-studio.com>
2018-08-22 17:05:45 +0300

    Base url change for frontend

--- src/client/helpers/api.js
+++ src/client/helpers/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import dateHelpers from "./date-helpers";
 import cookies from "./cookie-tool";
 // TODO: implement servers url
-const baseUrl = "http://127.0.0.1:5000";
+const baseUrl = "/";
 
 class Api {
     constructor() {
