1fdb178edd01b8b0841701efab622abeebd085b6
Vladyslav Sorochynskyi <vladyslav.sorochynskyi@gmail.com>
2018-08-23 12:03:54 +0300

    index js pretty, import auth-hoc

--- src/client/index.js
+++ src/client/index.js
@@ -1,27 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "whatwg-fetch";
-import {createStore, applyMiddleware} from "redux";
-import {Provider} from "react-redux";
-import {composeWithDevTools} from "redux-devtools-extension";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import "client/styles/global.scss";
 import reducer from "client/logic/reducer";
 
 import RegistrationPage from "client/pages/registration-page";
 import PropertyCreationTabs from "client/pages/add-property-page";
-import {Router, Route, Switch} from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import CheckInCheckOut from "client/pages/checkin-checkout-page";
-import {HomePage} from "client/pages/home-page";
+import { HomePage } from "client/pages/home-page";
 import PropertyPage from "client/pages/property-page";
 import LoginPage from "client/pages/login-page";
 import SearchPage from "client/pages/search-page";
-import {NotFoundPage} from "client/pages/404-page";
+import { NotFoundPage } from "client/pages/404-page";
 import createSagaMidddelware from "redux-saga";
 import rootSaga from "client/logic/rootSaga";
 import history from "client/history";
 import PhotoTab from "./components/photo-tab-registration-property";
 import UserCabinet from "./components/user-cabinet";
+import AuthHOC from "./components/auth-hoc";
 
 const sagaMiddelware = createSagaMidddelware();
 const middleware = [sagaMiddelware];
@@ -36,21 +37,21 @@ ReactDOM.render(
     <Provider store={store}>
         <Router history={history}>
             <Switch>
-                <Route exact path="/" component={HomePage}/>
-                <Route exact path="/signup" component={RegistrationPage}/>
-                <Route exact path="/login" component={LoginPage}/>
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/signup" component={RegistrationPage} />
+                <Route exact path="/login" component={LoginPage} />
                 <Route
                     exact
                     path="/checkin-checkout"
                     component={CheckInCheckOut}
                 />
-                <Route path="/search-page" component={SearchPage}/>
-                <Route path="/property-page" component={PropertyPage}/>
-                <Route path="/add-property/" component={PropertyCreationTabs}/>
-                <Route path="/404" component={NotFoundPage}/>
-                <Route path="/user-cabinet" component={UserCabinet}/>
+                <Route path="/search-page" component={SearchPage} />
+                <Route path="/property-page" component={PropertyPage} />
+                <Route path="/add-property/" component={PropertyCreationTabs} />
+                <Route path="/404" component={NotFoundPage} />
+                <Route path="/user-cabinet" component={UserCabinet} />
 
-                <Route component={NotFoundPage}/>
+                <Route component={NotFoundPage} />
             </Switch>
         </Router>
     </Provider>,
