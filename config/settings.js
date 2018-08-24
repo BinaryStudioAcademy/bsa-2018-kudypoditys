7a312e3ad1ea54e99dc2934d0fdeeaedd406f95c
unknown <gekag14@gmail.com>
2018-08-24 09:26:04 +0300

    Fixed token life time

--- config/settings.js
+++ config/settings.js
@@ -2,6 +2,6 @@ module.exports = {
     jwtPrivateKey: 'mySecret',
     accessTokenLife: 30 * 60, //30minute*60seconds
     refreshTokenLife: 30 * 24 * 60, //30days*24hours*60second
-    verifyEmailTokenLife: 30 * 60 * 1000, // 30 min * 60 sec
+    verifyEmailTokenLife: 30 * 60, // 30 min * 60 sec
     jwtRefreshTokenPrivateKey: 'myRefreshSecret'
 };
