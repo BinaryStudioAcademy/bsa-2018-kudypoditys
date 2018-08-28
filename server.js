4076b4b43e9d4ea1bcedc8f92fccf8c06f2e56d9
Bohdan Rusinka <bohdan.rusinka@binary-studio.com>
2018-08-28 13:32:24 +0300

    Fixed CORS error

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ require(`${apiRoot}/helpers/passport`);
 
 const port = 5000;
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
