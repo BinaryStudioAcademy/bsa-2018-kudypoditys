821792666897c5973df237560eb6e692f41bd8ba
dmitry-skrypka <dimitry.skripka@gmail.com>
2018-08-28 12:52:22 +0300

    basic save

--- src/client/components/property-payment-tab/index.jsx
+++ src/client/components/property-payment-tab/index.jsx
@@ -1,120 +1,144 @@
-import React, { Component } from "react";
-import { Container, Header, Segment, Radio, Divider, Dropdown, Input, Checkbox, Label } from "semantic-ui-react";
-import "./index.scss";
+import React, {Component} from 'react';
+import {
+    Container,
+    Header,
+    Segment,
+    Radio,
+    Divider,
+    Dropdown,
+    Input,
+    Checkbox,
+    Label,
+} from 'semantic-ui-react';
+import './index.scss';
 import { connect } from 'react-redux';
 import { mapStateToProps, mapDispatchToProps } from './container';
-import { commissionNames, cityTaxTypes, additionalFeeTypes } from "./staticData";
-import TabForm from "./servicesTabForm";
+import {
+    commissionNames,
+    cityTaxTypes,
+    additionalFeeTypes,
+} from './staticData';
+import TabForm from './servicesTabForm';
 
 export class PaymentTab extends Component {
-
     handleChange = (e, { name, value }) => {
-        if(value === "true") {
-            value = true
-        }
-        else if (value === "false") {
-            value = false
+        if (value === 'true') {
+            value = true;
+        } else if (value === 'false') {
+            value = false;
         }
         this.props.updateTab({ [name]: value });
     };
 
-    toggleCreditCardsCheck = (card) => {
+    toggleCreditCardsCheck = card => {
         this.props.updateTab({
             paymentCreditCards: {
                 ...this.props.paymentCreditCards,
-                [card]: !this.props.paymentCreditCards[card]
-            }
+                [card]: !this.props.paymentCreditCards[card],
+            },
         });
     };
 
     handleCityTaxChange = (e, {name, value}) => {
-        if(name !== 'includeToPrice') {
+        if (name !== 'includeToPrice') {
             this.props.updateTab({
                 cityTaxIndividual: {
                     ...this.props.cityTaxIndividual,
-                    [name]: value
-                }
+                    [name]: value,
+                },
             });
-        } else if(name === 'includeToPrice'){
+        } else if (name === 'includeToPrice') {
             this.props.updateTab({
                 cityTaxIndividual: {
                     ...this.props.cityTaxIndividual,
-                    includeToPrice: !this.props.cityTaxIndividual.includeToPrice
-                }
+                    includeToPrice: !this.props.cityTaxIndividual
+                        .includeToPrice,
+                },
             });
         }
     };
 
-    handleCommissionInput = (e, { name, value}) => {
+    handleCommissionInput = (e, {name, value}) => {
         this.props.updateTab({
             recipientActualAddress: {
                 ...this.props.recipientActualAddress,
-                [name]: value
-            }
+                [name]: value,
+            },
         });
     };
 
-    addFee = (fee) => {
+    addFee = fee => {
         this.props.updateTab({
             additionalFeesOptions: [
                 ...this.props.additionalFeesOptions,
                 {
                     feeType: this.additionalFeeTypes[0].value,
                     includeToPrice: false,
-                    value: "",
-                    paymentType: this.cityTaxTypes[0].value
-                }
-            ]
-        })
+                    value: '',
+                    paymentType: this.cityTaxTypes[0].value,
+                },
+            ],
+        });
     };
 
     handleAdditionalFees = (e, { name, value }) => {
         console.log(e, name, value);
-        const id =  Number(name.split(" ")[0]);
-        const name_ = name.split(" ")[1];
-        if(value === "true") {
+        const id = Number(name.split(' ')[0]);
+        const name_ = name.split(' ')[1];
+        if (value === 'true') {
             value = true;
-        } else if(value === "false") {
+        } else if (value === 'false') {
             value = false;
         }
 
-        if(!name.includes("removeFee") && !name.includes("includeToPrice")) {
+        if (!name.includes('removeFee') && !name.includes('includeToPrice')) {
             this.props.updateTab({
-                additionalFeesOptions: this.props.additionalFeesOptions.map((option, i) => {
-                    if(i == id) {
-                        return {
-                            ...option,
-                            [name_]: value
+                additionalFeesOptions: this.props.additionalFeesOptions.map(
+                    (option, i) => {
+                        if (i == id) {
+                            return {
+                                ...option,
+                                [name_]: value,
+                            };
+                        } else {
+                            return option;
                         }
-                    } else {
-                        return option;
-                    }
-                })
-            })
-        } else if(name.includes("includeToPrice")) {
+                    },
+                ),
+            });
+        } else if (name.includes('includeToPrice')) {
             this.props.updateTab({
-                additionalFeesOptions: this.props.additionalFeesOptions.map((option, i) => {
-                    if(i == id) {
-                        return {
-                            ...option,
-                            includeToPrice: !option.includeToPrice
+                additionalFeesOptions: this.props.additionalFeesOptions.map(
+                    (option, i) => {
+                        if (i == id) {
+                            return {
+                                ...option,
+                                includeToPrice: !option.includeToPrice,
+                            };
+                        } else {
+                            return option;
                         }
-                    } else {
-                        return option;
-                    }
-                })
-            })
+                    },
+                ),
+            });
         } else {
             this.props.updateTab({
-                additionalFeesOptions: this.props.additionalFeesOptions.filter((option, i) => i != id)
-            })
+                additionalFeesOptions: this.props.additionalFeesOptions.filter(
+                    (option, i) => i != id,
+                ),
+            });
         }
     };
 
     handleContinue = () => {
-        console.log(this.props)
-        console.log(this.props.name)
-        this.props.registerProperty({name: this.props.name})
+        console.log(this.props);
+        console.log(this.props.name);
+        this.props.registerProperty({
+            name: this.props.name,
+            address: this.props.address,
+            contactPersonName: this.props.contactPersonName,
+            contactPhone: this.props.contactPhone,
+        });
     };
 
     render() {
@@ -138,17 +162,20 @@ export class PaymentTab extends Component {
             additionalFeesOptions: this.props.additionalFeesOptions,
             commissionName: this.props.commissionName,
             recipientSameAddress: this.props.recipientSameAddress,
-            recipientActualAddress: this.props.recipientActualAddress
+            recipientActualAddress: this.props.recipientActualAddress,
         };
 
         return (
-            <Container className='property_payment_tab-wrapper'>
-                <Container className='property_payment_tab-container'>
+            <Container className="property_payment_tab-wrapper">
+                <Container className="property_payment_tab-container">
                     <TabForm {...formProps} onSubmit={this.handleContinue} />
                 </Container>
             </Container>
-        )
+        );
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(PaymentTab);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(PaymentTab);
