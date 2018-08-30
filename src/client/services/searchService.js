b71f740a0883a74b378bb9bbe8ed717f51bbb2ab
fediuchkoelvira <fediuchko.elvira@gmail.com>
2018-08-30 22:13:34 +0300

    fixed saga for search

--- src/client/services/searchService.js
+++ src/client/services/searchService.js
@@ -1,25 +1,16 @@
-import api from '../helpers/api';
-import history from 'client/history';
+import api from "../helpers/api";
+
 
 class SearchService {
     submitSearch(data) {
-        console.log("SearchService submitSearch  - "+JSON.stringify(data))
-        return api.sendRequest('/elastic/search/?index=properties&type=document&query='+data.query, 'get',"")//.then(response => {
-        //  //  if(response.status === 200){
-        //      //  history.push('/');
-        //  // }4
-        //  console.log(JSON.stringify(response))
-
-        //  return response
-        //     // response
-        // });
+        console.log("SearchService submitSearch  - " + JSON.stringify(data));
+        return api.sendRequest(
+            "/elastic/search/?index=properties&type=document&query=" +
+                data.query,
+            "get",
+            ""
+        );
     }
-    // updateSearch(data) {
-    //     return api.sendRequest(`/api/search/`, 'put', data).then(response => {
-    //         history.push('/search/');
-    //     });
-    // }
-
 }
 
 export default new SearchService();
