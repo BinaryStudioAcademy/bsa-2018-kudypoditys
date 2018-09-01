42680bb982db9ace0b1c1e323bd3ebe9ee9312f3
unknown <gekag14@gmail.com>
2018-09-01 11:25:01 +0300

    Merged beta

--- src/client/logic/property-page/reducer.js
+++ src/client/logic/property-page/reducer.js
@@ -1,14 +1,30 @@
-import defaultState from 'client/logic/defaultState';
+import defaultState from "client/logic/defaultState";
+import {
+    GET_PROPERTY_INFO_SUCCESS,
+    AVAILABILITY_INPUT_UPDATE
+} from "./actionTypes";
 
-
-function propertyPageReducer(state = defaultState.shownProperties, action) {
+function propertyPageReducer(state = defaultState.propertyPage, action) {
     switch (action.type) {
-
+        case GET_PROPERTY_INFO_SUCCESS: {
+            return {
+                ...state,
+                property: action.payload
+            };
+        }
+        case AVAILABILITY_INPUT_UPDATE: {
+            return {
+                ...state,
+                availabilityInput: {
+                    ...state.availabilityInput,
+                    ...action.payload
+                }
+            };
+        }
         default: {
             return state;
         }
     }
-
 }
 
 export default propertyPageReducer;
--- src/client/pages/property-page/index.jsx
+++ src/client/pages/property-page/index.jsx
@@ -1,46 +1,68 @@
 import React from "react";
 import "./index.scss";
-import {Divider, Container, Segment, Breadcrumb} from "semantic-ui-react";
-import {connect} from "react-redux";
-import {mapStateToProps, mapDispatchToProps} from "./container";
+import { Divider, Container, Segment, Breadcrumb } from "semantic-ui-react";
+import { connect } from "react-redux";
+import { mapStateToProps, mapDispatchToProps } from "./container";
 import Search from "client/components/search";
 import Header from "client/components/header";
 import AvailabilityPanel from "client/components/availability-panel";
 import Slider from "client/components/slider";
 import PropertyDescription from "client/components/property-description";
-import {PropertySummary} from "client/components/property-summary";
-import {NavigationBar} from "client/components/navigation-bar";
+import { PropertySummary } from "client/components/property-summary";
+import { NavigationBar } from "client/components/navigation-bar";
 import BasicMapWidget from "client/components/basic-map-widget";
 import RoomsSummaryTable from "client/components/rooms-summary-table";
 export class PropertyPage extends React.Component {
-    render() {
-        let propertyItemData = {
-            name: "DREAM Hostel Lviv",
-            location: "Prospekt Gagarina 145, Lviv, 61124, Ukraine "
-        };
-        const pics = [
-            "https://www.hotelimperialeroma.it/data/mobile/hotel-imperiale-roma-camere-01-2.jpg",
-            "https://www.hotelimperialeroma.it/data/jpg/hotel-imperiale-rome-8.jpg",
-            "https://www.hotelimperialeroma.it/data/jpg/hotel-imperiale-rome-10.jpg",
-            "https://www.hotelimperialeroma.it/data/jpg/hotel-imperiale-rome-11.jpg",
-            "https://www.hotelimperialeroma.it/data/jpg/hotel-imperiale-rome-12.jpg"
-        ];
+    componentWillMount() {
+        this.props.getProperty(this.props.match.params.id);
+    }
 
-        const sections = [
-            {key: "Main", content: "Main", link: true},
-            {key: "Country", content: "Ukraine", link: true},
-            {key: "Region", content: "Lviv Region", link: true},
-            {key: "City", content: "Lviv", link: true},
-            {key: "Property", content: "DREAM Hostel Lviv", active: true}
-        ];
+    getImagesArray(propertyImages) {
+        let images = [];
+        for (let i = 0; i < propertyImages.length; i++) {
+            images.push(propertyImages[i].url);
+        }
+        return images;
+    }
+
+    render() {
+        const { property } = this.props;
 
         const handleSlideChange = index => {
             console.log(`Slide changed to ${index}`);
         };
 
+        if (!property) return null;
+
+        const pics = this.getImagesArray(property.images);
+        const sections = [
+            { key: "Home", content: "Home", href: "/" },
+            {
+                key: "Country",
+                content: `${property.city.country.name}`,
+                href: "#"
+            },
+            {
+                key: "Region",
+                content: `${property.city.name} Region`,
+                href: "#"
+            },
+            {
+                key: "City",
+                content: property.city.name,
+                href: "#"
+            },
+            {
+                key: "Property",
+                content: property.name,
+                link: false,
+                active: true
+            }
+        ];
+
         return (
             <div className="mock">
-                <Header showSearch={true}/>
+                <Header showSearch={true} />
                 <div className="property-page__wrapper">
                     <Container
                         text
@@ -48,7 +70,7 @@ export class PropertyPage extends React.Component {
                     >
                         <BasicMapWidget
                             key="BasicMapWidget"
-                            location={{lat: 49.837089, lng: 24.021161}}
+                            location={property.coordinates}
                             rounded
                             centered
                         />
@@ -58,29 +80,29 @@ export class PropertyPage extends React.Component {
                         text
                         className="property-page__wrapper-right_side"
                     >
-                        <NavigationBar/>
+                        <NavigationBar />
 
-                        <PropertySummary propertyItemData={propertyItemData}/>
+                        <PropertySummary property={property} />
                         <Slider
                             pics={pics}
                             handleSlideChange={handleSlideChange}
                             slideIndex={0}
                         />
 
-                        <Divider hidden/>
+                        <Divider hidden />
                         <div
                             className="property-page__description"
-                            style={{width: "100%"}}
+                            style={{ width: "100%" }}
                         >
                             <PropertyDescription
-                                id="xyz-1"
-                                style={{width: "100%"}}
+                                property={property}
+                                style={{ width: "100%" }}
                             />
                         </div>
-                        <Divider hidden/>
+                        <Divider hidden />
 
-                        <AvailabilityPanel style={{width: "100%"}}/>
-                        <RoomsSummaryTable/>
+                        <AvailabilityPanel style={{ width: "100%" }} />
+                        <RoomsSummaryTable rooms={property.rooms} />
                     </Container>
                 </div>
             </div>
