f978a18c57dc25d93e744d74a0c5dd86b04de711
Vladyslav Sorochynskyi <vladyslav.sorochynskyi@gmail.com>
2018-09-04 17:40:02 +0300

    booking input clearing fix

--- src/client/pages/property-page/container.jsx
+++ src/client/pages/property-page/container.jsx
@@ -4,10 +4,9 @@ import {
 } from "../../logic/property-page/actions";
 import { getUserBookings } from "../../logic/user-cabinet/actions";
 
-
 export function mapStateToProps(state, ownProps) {
-    const {user} = state.userCabinet;
-    const {property} = state.propertyPage;
+    const { user } = state.userCabinet;
+    const { property } = state.propertyPage;
 
     return {
         property: property,
@@ -24,7 +23,14 @@ export function mapDispatchToProps(dispatch, ownProps) {
             dispatch(getUserBookings());
         },
         clearBookingForm() {
-            dispatch(bookingInputUpdate({message: "", error: ""}));
+            dispatch(
+                bookingInputUpdate({
+                    message: "",
+                    error: "",
+                    checkIn: null,
+                    checkOut: null
+                })
+            );
         }
     };
 }
