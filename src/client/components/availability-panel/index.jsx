cdd7c4d83d463b5c4f0c831221995be3abbd78e4
Vladyslav Sorochynskyi <vladyslav.sorochynskyi@gmail.com>
2018-09-08 16:16:11 +0300

    close icon in availability panel modal

--- src/client/components/availability-panel/index.jsx
+++ src/client/components/availability-panel/index.jsx
@@ -179,6 +179,7 @@ export class AvailabilityPanel extends React.Component {
                                     />
                                 </div>
                             }
+                            closeIcon
                         >
                             {error ? (
                                 <Message negative>{error}</Message>
