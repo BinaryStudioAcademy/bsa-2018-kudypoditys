04761ce7e96f97e3f3cacda1b0ce8e0a00d71e36
Vladyslav Sorochynskyi <vladyslav.sorochynskyi@gmail.com>
2018-09-08 13:43:37 +0300

    merge remote tracking from feature/property-page

--- src/client/components/property-registration/DrawTab.jsx
+++ src/client/components/property-registration/DrawTab.jsx
@@ -4,12 +4,11 @@ import { Header, Tab } from "semantic-ui-react";
 export class DrawTab extends Component {
 
     render() {
-        const { header, subheader, component } = this.props;
+        const { customHeader, subheader, component } = this.props;
         return (
             <Tab.Pane>
-
                 <Header as="h2">
-                    {header}
+                    {customHeader}
                 </Header>
                 <Header as="h4" style={{ paddingBottom: 10, lineHeight: 1.2 }}>
                     {subheader}
--- src/client/components/quick-filter/filters.js
+++ src/client/components/quick-filter/filters.js
@@ -11,3 +11,107 @@ let filters = [
     {id: '10', ischecked: false, label: 'Unrated', amount: 658, type: 'Hotel Class'},
     {id: '11', ischecked: true, label: 'Pool2', amount: 321, type: 'Facility'},
 ]
+
+export const ratingScore = [
+    {
+        name: 'rating5',
+        key: 'rating5',
+        label: '5',
+    },
+    {
+        name: 'rating4',
+        key: 'rating4',
+        label: '4',
+    },
+    {
+        name: 'rating3',
+        key: 'rating3',
+        label: '3',
+    },
+    {
+        name: 'rating2',
+        key: 'rating2',
+        label: '2',
+    },
+    {
+        name: 'rating1',
+        key: 'rating1',
+        label: '1',
+    },
+    {
+        name: 'ratingUndefined',
+        key: 'ratingUndefined',
+        label: 'Unrated',
+    },
+];
+
+export const facilities = [
+    {
+        name: 'wellness_facilities',
+        key: 'Wellness facilities',
+        label: 'Wellness facilities',
+    },
+    {
+        name: 'pets',
+        key: 'pets',
+        label: 'Pets',
+    },
+    {
+        name: 'outdoors',
+        key: 'outdoors',
+        label: 'Outdoors',
+    },
+    {
+        name: 'cleaning_services',
+        key: 'rating2',
+        label: 'Cleaning services',
+    },
+    {
+        name: 'gctivities',
+        key: 'activities',
+        label: 'Activities',
+    },
+    {
+        name: 'general',
+        key: 'general',
+        label: 'General',
+    },
+];
+
+export const bedTypes = [
+    {
+        name: 'twin_bed',
+        key: 'wellness_facilities',
+        label: 'Wellness facilities',
+    },
+    {
+        name: 'full_bed',
+        key: 'full_bed',
+        label: 'Full bed',
+    },
+    {
+        name: 'queen_bed',
+        key: 'queen_bed',
+        label: 'Queen bed',
+    },
+    {
+        name: 'king_bed',
+        key: 'king_bed',
+        label: 'King bed',
+    },
+    {
+        name: 'bunk_bed',
+        key: 'bunk_bed',
+        label: 'Bunk bed',
+    },
+    {
+        name: 'sofa_bed',
+        key: 'sofa_bed',
+        label: 'Sofa bed',
+    },
+    {
+        name: 'futon_bed',
+        key: 'futon_bed',
+        label: 'Futon bed',
+    },
+];
