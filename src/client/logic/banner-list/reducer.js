bca1a9adc6f9add22547038f30f166be7c7a9aa0
Dmytro Kostiuk <dmytro.kostiuk@binary-studio.com>
2018-09-14 10:38:07 +0300

    Change default city infos.

--- src/client/logic/banner-list/reducer.js
+++ src/client/logic/banner-list/reducer.js
@@ -52,8 +52,8 @@ const CITY_INFOS = {
     Lviv: {
         id: 1,
         name: 'Lviv',
-        properties: 4098,
-        avgPrice: 12012,
+        properties: 8,
+        avgPrice: 120,
         imageUrl:
             'http://www.mgi4ua.com/wp-content/uploads/2017/11/lviv-ukraine.jpg',
         flagUrl:
@@ -62,8 +62,8 @@ const CITY_INFOS = {
     Dnipro: {
         id: 6,
         name: 'Dnipro',
-        properties: 202,
-        avgPrice: 112,
+        properties: 3,
+        avgPrice: 22,
         imageUrl:
             'http://meandyoukraine.com/mainContent/DniproCity/DniproCity_featuredImage.jpg',
         flagUrl:
@@ -72,8 +72,8 @@ const CITY_INFOS = {
     Ternopil: {
         id: 3,
         name: 'Ternopil',
-        properties: 1202,
-        avgPrice: 1012,
+        properties: 3,
+        avgPrice: 28,
         imageUrl:
             'http://www.gazeta-misto.te.ua/wp-content/uploads/2017/05/18671255_1124933304279283_1785861677540967562_n.jpg',
         flagUrl:
@@ -82,8 +82,8 @@ const CITY_INFOS = {
     Kiev: {
         id: 2,
         name: 'Kiev',
-        properties: 92202,
-        avgPrice: 182032,
+        properties: 7,
+        avgPrice: 32,
         imageUrl: 'https://s.inyourpocket.com/gallery/130361.jpg',
         flagUrl:
             'http://proudofukraine.com/wp-content/uploads/2015/06/Ukrainian-flag.png',
@@ -91,8 +91,8 @@ const CITY_INFOS = {
     Odessa: {
         id: 4,
         name: 'Odessa',
-        properties: 5602,
-        avgPrice: 2082,
+        properties: 4,
+        avgPrice: 24,
         imageUrl:
             'https://www.hotel-deribas.com/wp-content/uploads/2018/03/19odessa.jpg',
         flagUrl:
@@ -101,8 +101,8 @@ const CITY_INFOS = {
     Kharkiv: {
         id: 5,
         name: 'Kharkiv',
-        properties: 602,
-        avgPrice: 282,
+        properties: 2,
+        avgPrice: 27,
         imageUrl:
             'http://www.yoldasin.com/wp-content/uploads/2017/04/kharkiv-tren-istasyonu-960x638.jpg',
         flagUrl:
