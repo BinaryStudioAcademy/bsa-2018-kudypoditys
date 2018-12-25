0df919c9d0a8b430e67a7e1235d622a67f7a5845
bohdan konorin <bohdan.konorin@binary-studio.com>
2018-12-25 15:04:54 +0200

    Added info about metro and centre distance

--- src/api/models/Property.js
+++ src/api/models/Property.js
@@ -19,6 +19,12 @@ let Property = orm.define('property', {
         type: Sequelize.FLOAT,
         validate: { notEmpty: true }
     },
+    distanceToMetro: {
+        type: Sequelize.FLOAT
+    },
+    nearestMetro: {
+        type: Sequelize.STRING
+    },
     lastBooked : {
         type : Sequelize.DATE,
     },
