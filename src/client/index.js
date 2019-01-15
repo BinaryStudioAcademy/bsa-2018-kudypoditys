b57b74063143a089b8cd4e03641ec2a387f5a3c2
bogdaner2 <KonorinDeveloper@gmail.com>
2019-01-15 09:40:07 +0200

    Fixed bug with routing after refactoring

--- src/client/index.js
+++ src/client/index.js
@@ -2,17 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "whatwg-fetch";
 import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 import "client/styles/global.scss";
 import reducer from "client/logic/reducer";
-import { Router } from "react-router-dom";
 import createSagaMidddelware from "redux-saga";
 import rootSaga from "client/logic/rootSaga";
-import history from "client/history";
-import SimpleModal from 'client/components/simple-modal';
-import ErrorBoundary from "client/components/error-boundary-handler";
 import {socket} from "./logic/socket"
 import Root from "./components/root-component";
 
@@ -25,18 +20,7 @@ const store = createStore(
 );
 sagaMiddelware.run(rootSaga);
 
-ReactDOM.render(
-    <Provider store={store}>
-        <ErrorBoundary>
-            <Router history={history}>
-                <Root>
-                </Root>
-            </Router>
-            <SimpleModal />
-        </ErrorBoundary>
-    </Provider>,
-    document.getElementById("root")
-);
+ReactDOM.render(<Root store={store}/>, document.getElementById("root"));
 
 window.addEventListener("beforeunload", (ev) =>
 {
