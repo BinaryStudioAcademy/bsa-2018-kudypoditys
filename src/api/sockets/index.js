18741f4fd16ddf833180c97ea18eb70aeeac2b3e
bogdaner2 <KonorinDeveloper@gmail.com>
2019-01-16 16:12:34 +0200

    Fixed issue with traced same user in different tabs

--- src/api/sockets/index.js
+++ src/api/sockets/index.js
@@ -1,30 +1,42 @@
 module.exports = io => {
 
+    let roomSockets = []
+
     io.sockets.on("connection", (socket) => {
 
         let data = socket.conn.request._query;
+
+        roomSockets = roomSockets.concat({ socketId : socket.id, token : data.token, roomId : data.roomId })
 
         socket.join("room " + data.roomId)
 
         io
         .in("room "  + data.roomId)
-        .clients((error, clients) => {
-               io
-               .in("room " + data.roomId)
-               .emit("nowLooking", clients.length - 1);
-            });
+        .emit("nowLooking",
+                [...new Set(
+                    roomSockets
+                    .filter(x => x.roomId === data.roomId)
+                    .map(x => x.token))
+                ]
+                .length - 1);
 
         socket.on("disconnect", () => {
 
-            socket.leave("room "  + data.roomId);
+            const roomId = socket.conn.request._query.roomId;
+
+            roomSockets = roomSockets.filter(x => !(x.socketId === socket.id));
+
+            socket.leave("room " + roomId);
 
             io
-            .in("room "  + data.roomId)
-            .clients((error, clients) => {
-                   io
-                   .in("room " + socket.conn.request._query.roomId)
-                   .emit("nowLooking", clients.length - 1);
-                });
+            .in("room "  + roomId)
+            .emit("nowLooking",
+                    [...new Set(
+                        roomSockets
+                        .filter(x => x.roomId === roomId)
+                        .map(x => x.token))
+                    ]
+                    .length - 1);
 
             socket.disconnect();
         });
